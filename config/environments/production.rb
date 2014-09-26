37d25674d724221d141aefc9ceaf6a0b268bbf04
icyleaf <icyleaf.cn@gmail.com>
2014-09-26 10:13:43 +0800

    rails_apps_composer: set email accounts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,22 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
+
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
