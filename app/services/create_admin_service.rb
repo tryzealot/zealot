cf7ef057f7ef1a2d04817278e658c609a19c0beb
icyleaf <icyleaf.cn@gmail.com>
2014-09-26 10:14:14 +0800

    rails_apps_composer: set up database

---
+++ app/services/create_admin_service.rb
@@ -0,0 +1,8 @@
+class CreateAdminService
+  def call
+    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+        user.password = Rails.application.secrets.admin_password
+        user.password_confirmation = Rails.application.secrets.admin_password
+      end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user = CreateAdminService.new.call
+puts 'CREATED ADMIN USER: ' << user.email
