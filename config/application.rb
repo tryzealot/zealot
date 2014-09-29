2a327290d3c570ff037463f881e3b0f2fe3f19f1
icyleaf <icyleaf.cn@gmail.com>
2014-09-29 14:05:21 +0800

    支持 emoji

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,9 @@ module Im
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.paths << Emoji.images_path
+
+    config.assets.precompile << "emoji/**/*.png"
   end
 end
