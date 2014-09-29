2ced270e17eb9d99bbbe655188c1fe61ff1385c1
icyleaf <icyleaf.cn@gmail.com>
2014-09-29 17:20:37 +0800

    如果数据库存了记录就不在继续查找

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,17 @@
 class VisitorsController < ApplicationController
+  protect_from_forgery with: :null_session
+  respond_to :json
+
+  def upload
+ #  	gzip = ActiveSupport::Gzip.compress('compress me!')
+	ungzip = ActiveSupport::Gzip.decompress(request.raw_post)
+
+	data = MultiJson.load request.raw_post
+	# raw = ActiveSupport::Gzip.decompress('compress me!')
+ 	render json: {
+   		# gzip: gzip,
+   		# ungzip: ungzip,
+   		body: data
+    }
+  end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,17 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'mobile'
+set :repo_url, 'git@gitlab.dev:icyleaf/mobile.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :deploy_to, '/var/www/mobile'
+set :scm, :git
 
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
 
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
