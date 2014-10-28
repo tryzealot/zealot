ad532583b8750718a8b55571b6b2bd9e0d2828a4
icyleaf <icyleaf.cn@gmail.com>
2014-10-28 20:28:19 +0800

    支持 文件 上传

--- app/controllers/api/v1/binary_controller.rb
+++ app/controllers/api/v1/binary_controller.rb
@@ -1,17 +1,12 @@
 class Api::V1::BinaryController < Api::ApplicationController
 
   def ipa
-    file = params.delete :file
+    file = params.delete :dsym
+
+    FileUtils.mkdir_p "public/uploads/ipa"
 
     if file.is_a?(ActionDispatch::Http::UploadedFile)
-      # temp_file = Tempfile.new(file.original_filename)
-
-      # temp_file.write file.read
-      # file = temp_file
-      # temp_file.close
-
-
-      File.open("public/uploads/ipa//#{file.original_filename}", "wb") { |f| f.write(file.read) }
+      File.open("public/uploads/ipa/#{file.original_filename}", "wb") { |f| f.write(file.read) }
     end
 
     render json: {
@@ -20,7 +15,7 @@ class Api::V1::BinaryController < Api::ApplicationController
   end
 
   def apk
-
+    FileUtils.mkdir_p "public/uploads/apk"
   end
 
 end
