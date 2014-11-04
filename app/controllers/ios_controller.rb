e1c13bfd77732735abf07404ccabe2b42a3d8989
icyleaf <icyleaf.cn@gmail.com>
2014-11-04 18:14:30 +0800

    支持 dsym 的记录删除

--- app/controllers/ios_controller.rb
+++ app/controllers/ios_controller.rb
@@ -7,6 +7,14 @@ class IosController < ApplicationController
     @ioses = Ios.order('created_at DESC').all
   end
 
+  def destroy
+    ios = Ios.find(params[:id])
+    dsym_file = "public/uploads/ipa/#{ios.dsym_file}"
+    File.delete(dsym_file) if File.exists? dsym_file
+    ios.destroy
+    redirect_to :back
+  end
+
   def download
     ios = Ios.find(params[:id])
 
