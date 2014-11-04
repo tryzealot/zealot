1d80291da38d8d7784000b4c75a41f72eec3e811
icyleaf <icyleaf.cn@gmail.com>
2014-11-04 18:14:30 +0800

    支持 dsym 的记录删除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :ios
+
   get 'ios/download/:id', to: 'ios#download', as: 'ios_download'
+  resources :ios
 
   devise_for :users
   get 'users/chatroom', to: 'users#chatrooms', as: 'user_chatrooms'
