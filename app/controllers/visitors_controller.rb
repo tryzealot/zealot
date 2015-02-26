d7bc87e7ae27df8adf0b6a3151831e6dddfa29f3
icyleaf <icyleaf.cn@gmail.com>
2015-02-26 10:42:41 +0800

    修改用户资料页面

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,3 +1,10 @@
 class VisitorsController < ApplicationController
+  # before_filter :authenticate_user!
+
+  def index
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
 
 end
