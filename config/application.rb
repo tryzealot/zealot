3cf526c4db861000460e98338604c07a96f9ec42
icyleaf <icyleaf.cn@gmail.com>
2015-03-06 13:35:21 +0800

    补充部分中文化文件

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Im
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :"zh-CN"
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
