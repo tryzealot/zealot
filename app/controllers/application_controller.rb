bdf21781477654e7de1d425ffa8b41423773b9f2
icyleaf <icyleaf.cn@gmail.com>
2015-03-25 12:07:30 +0800

    试试修复因需要登录跳转

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,15 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
   # before_filter :detect_mobile
 
+
+  protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referer || root_path
+    end
+  end
 end
