394369cb068984dca54d502e5f1145386393de1d
icyleaf <icyleaf.cn@gmail.com>
2015-07-01 18:13:13 +0800

    显示 app 大小

--- app/models/release.rb
+++ app/models/release.rb
@@ -3,6 +3,23 @@ class Release < ActiveRecord::Base
 
   before_create :auto_release_version
 
+  def filesize
+    fileext = case app.device_type.downcase
+    when 'iphone', 'ipad', 'ios'
+      '.ipa'
+    when 'android'
+      '.apk'
+    end
+
+    File.size(File.join(
+      "public/uploads/apps",
+      app.user.id.to_s,
+      app_id.to_s,
+      "#{id.to_s}#{fileext}"
+    ))
+  end
+
+
   private
     def auto_release_version
       latest_version = Release.where(app_id:self.app_id).last
