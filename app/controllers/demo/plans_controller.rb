1e5688a92d020b9f050d8d75997e5240843b6a44
icyleaf <icyleaf.cn@gmail.com>
2015-08-13 17:05:49 +0800

    添加演示需要的界面，调整 navbar

---
+++ app/controllers/demo/plans_controller.rb
@@ -0,0 +1,14 @@
+class Demo::PlansController < ApplicationController
+
+  def index
+  end
+
+  def oneday
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ app/helpers/demo/plans_helper.rb
@@ -0,0 +1,2 @@
+module Demo::PlansHelper
+end
