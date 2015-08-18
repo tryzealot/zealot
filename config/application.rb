5781cd1f30570ca675f616111611ba2c8bd28fae
icyleaf <icyleaf.cn@gmail.com>
2015-08-18 17:10:41 +0800

    cleanup

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Im
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Beijing'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
