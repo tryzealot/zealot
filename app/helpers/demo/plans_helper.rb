ad528d5add95d5852f14e86e8f29dae3792bc6ea
icyleaf <icyleaf.cn@gmail.com>
2015-08-20 14:30:38 +0800

    处理上报用户坐标到 RA issue #4

--- app/helpers/demo/plans_helper.rb
+++ app/helpers/demo/plans_helper.rb
@@ -1,10 +1,10 @@
-require 'geo-distance'
+require 'haversine'
 
 
 module Demo::PlansHelper
 
   def geo_distance(from_lat, from_lon, to_lat, to_lon)
-    GeoDistance::Haversine.geo_distance(from_lat, from_lon, to_lat, to_lon).kms.round(2)
+    Haversine.distance(from_lat.to_f, from_lon.to_f, to_lat.to_f, to_lon.to_f)
   end
 
 end
