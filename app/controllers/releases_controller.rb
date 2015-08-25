f06f838efa57a830b59242de78f036208f1d7cf9
icyleaf <icyleaf.cn@gmail.com>
2015-08-25 18:48:01 +0800

    修复编辑app更新日志的跳转问题

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -17,6 +17,6 @@ class ReleasesController < ApplicationController
     fail ActionController::RoutingError.new('这里没有你找的东西') unless @release
 
     @release.update!(changelog: params[:changelog])
-    redirect_to app_release_path(@release.app.slug, @release.id)
+    redirect_to app_release_path(@release.app.slug, @release.version)
   end
 end
