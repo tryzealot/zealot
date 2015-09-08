f783296d5c0b80e3b30f5dd1eba2866ec679d619
icyleaf <icyleaf.cn@gmail.com>
2015-09-08 14:29:54 +0800

    使用 qrcode 生成工具替换线上服务

--- app/helpers/app_helper.rb
+++ app/helpers/app_helper.rb
@@ -1,21 +1,29 @@
 module AppHelper
 
-  def qr_code(content, options = {})
-    m = options[:m] || 20
-    bg = options[:bg] || 'f4f5f6'
-    fg = options[:fg] || '475a62'
-    el = options[:el] ||  'h'
+  def qr_code(url, options = {})
+    qrcode = RQRCode::QRCode.new(url, level: :h)
+    raw qrcode.as_svg(
+      color: '465960',
+      fill: 'F4F5F6',
+      module_size: 7,
+      offset: 15,
+    )
 
-    api_url = 'http://qr.liantu.com/api.php'
-    api_query = {
-      m: m,
-      bg: bg,
-      fg: fg,
-      el: el,
-      text: content
-    }
-
-    image_tag api_url + "?" + api_query.to_query
+    # m = options[:m] || 20
+    # bg = options[:bg] || 'f4f5f6'
+    # fg = options[:fg] || '475a62'
+    # el = options[:el] ||  'h'
+    #
+    # api_url = 'http://qr.liantu.com/api.php'
+    # api_query = {
+    #   m: m,
+    #   bg: bg,
+    #   fg: fg,
+    #   el: el,
+    #   text: content
+    # }
+    #
+    # image_tag api_url + "?" + api_query.to_query
   end
 
 end
