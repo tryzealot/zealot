5f389f8ba4866a913a808f62409585ab7c29d107
icyleaf <icyleaf.cn@gmail.com>
2015-09-09 10:49:06 +0800

    测试 JSPatch

---
+++ public/assets/jspatch/alert.js
@@ -0,0 +1,5 @@
+var alertView = require('UIAlertView').alloc().init();
+alertView.setTitle('Alert');
+alertView.setMessage('AlertView from js');
+alertView.addButtonWithTitle('OK');
+alertView.show();
