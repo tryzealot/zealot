10d2294a5ba794bb4031a68b3aded617be9f8bce
icyleaf <icyleaf.cn@gmail.com>
2015-09-09 11:19:13 +0800

    测试 JSPatch 的方法替换

---
+++ public/assets/jspatch/replace.js
@@ -0,0 +1,11 @@
+defineClass("RecommendViewController", {
+  viewDidLoad: function() {
+    self.ORIGviewDidLoad();
+
+    var alertView = require('UIAlertView').alloc().init();
+    alertView.setTitle('重载方法');
+    alertView.setMessage('重载 viewDidiLoad');
+    alertView.addButtonWithTitle('OK');
+    alertView.show();
+  }
+}, {})
