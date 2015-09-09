d28262816c7fc4825116cfdbbe255d6b8ce95db9
icyleaf <icyleaf.cn@gmail.com>
2015-09-09 13:56:09 +0800

    添加补丁增加必备参数

--- app/models/jspatch.rb
+++ app/models/jspatch.rb
@@ -1,3 +1,8 @@
 class Jspatch < ActiveRecord::Base
   belongs_to :app
+
+  validates :app, presence: true
+  validates :title, presence: true
+  validates :filename, presence: true
+  validates :script, presence: true
 end
