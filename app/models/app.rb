1eb9f9b8ade52e84ab16233067f383e40c86d256
icyleaf <icyleaf.cn@gmail.com>
2015-10-23 16:33:27 +0800

    增加查看app历史版本号列表功能

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,4 +1,7 @@
 class App < ActiveRecord::Base
+  include FriendlyId
+  friendly_id :slug
+
   has_many :releases
   belongs_to :user
 
@@ -16,6 +19,21 @@ class App < ActiveRecord::Base
         .select { |m| ! m.name.to_s.empty? }
         .sort_by { |m| m.created_at }
         .reverse
+    end
+  end
+
+  def versions
+    Rails.cache.fetch("app_#{self.id}_versions", expires_in: 1.day) do
+      versions = []
+      self.releases
+        .select(["release_version AS name"])
+        .group(:release_version)
+        .order(created_at: :desc)
+        .each do |model|
+          versions.push model.name
+        end
+
+      versions
     end
   end
 
