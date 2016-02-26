586071a7ab2aa9f2a7ae89f0979fb19a39bae99e
icyleaf <icyleaf.cn@gmail.com>
2016-02-26 14:39:25 +0800

    fix: 修复更新 pac 方法缺失

--- app/controllers/pacs_controller.rb
+++ app/controllers/pacs_controller.rb
@@ -26,6 +26,20 @@ class PacsController < ApplicationController
     end
   end
 
+  # PATCH/PUT /jspatches/1
+  # PATCH/PUT /jspatches/1.json
+  def update
+    respond_to do |format|
+      if @pac.update(pac_params)
+        format.html { redirect_to @pac, notice: 'pac was successfully updated.' }
+        format.json { render :show, status: :ok, location: @pac }
+      else
+        format.html { render :edit }
+        format.json { render json: @pac.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   protected
     def js_request?
       request.format.js?
