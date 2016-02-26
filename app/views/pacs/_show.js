38530fee7d09dcfbcffd6f30b0a2d00a7b557abe
icyleaf <icyleaf.cn@gmail.com>
2016-02-26 15:33:53 +0800

    style: 调整样式

--- app/views/pacs/_show.js
+++ app/views/pacs/_show.js
@@ -1,12 +1,12 @@
 <% if @pac.is_enabled == 1 %>
-	// pac is enabled
-	<% if @pac.script.to_s.empty? %>
-		function FindProxyForURL(url, host) {
-			return "PROXY <%= @pac.host %>:<%= @pac.port %>; DIRECT";
-		}
-	<% else %>
-		<%= raw @pac.script %>
-	<% end %>
+// pac is enabled
+<% if @pac.script.to_s.empty? %>
+function FindProxyForURL(url, host) {
+	return "PROXY <%= @pac.host %>:<%= @pac.port %>; DIRECT";
+}
+<% else %>
+<%= raw @pac.script %>
+<% end %>
 <% else %>
 // pac is disabled
 function FindProxyForURL(url, host) {
