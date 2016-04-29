b00b069611d6a67ccfdaa75bdf94951ec0ddd187
icyleaf <icyleaf.cn@gmail.com>
2016-04-29 12:39:35 +0800

    style: 修改一下样式

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -20,13 +20,13 @@ module Im
     # -- all .rb files in that directory are automatically loaded.
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # Run 'rake -D time' for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Beijing'
     config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :"zh-CN"
+    config.i18n.default_locale = :'zh-CN'
 
     # config.exceptions_app = self.routes
 
@@ -35,10 +35,10 @@ module Im
 
     # Redis for cache
     config.cache_store = :redis_store, {
-      host: "localhost",
+      host: 'localhost',
       port: 6379,
       db: 0,
-      namespace: "qyer:mobile:web",
+      namespace: 'qyer:mobile:web',
       expires_in: 90.minutes
     }
 
@@ -46,18 +46,15 @@ module Im
 
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
 
-    config.assets.paths << Rails.root.join("lib", "assets", "javascripts")
+    config.assets.paths << Rails.root.join('lib', 'assets', 'javascripts')
 
     config.assets.paths << Emoji.images_path
-    config.assets.precompile << "emoji/**/*.png"
+    config.assets.precompile << 'emoji/**/*.png'
 
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    config.middleware.insert_before 0, 'Rack::Cors', debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins '*'
-        resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :options],
-          :max_age => 0
+        resource '*', headers: :any, methods: [:get, :post, :options], max_age: 0
       end
     end
   end
