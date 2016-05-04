198531542c17d426a01b9813c5a7f5c8d68d785f
icyleaf <icyleaf.cn@gmail.com>
2016-05-04 14:49:37 +0800

    整理 public 目录 assets

--- public/assets/jspatch/alert.js
+++
@@ -1,5 +0,0 @@
-var alertView = require('UIAlertView').alloc().init();
-alertView.setTitle('Alert');
-alertView.setMessage('AlertView from js');
-alertView.addButtonWithTitle('OK');
-alertView.show();
