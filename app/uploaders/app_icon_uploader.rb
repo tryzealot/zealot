8b22d98e52486207310caabcb598227cd6663892
icyleaf <icyleaf.cn@gmail.com>
2016-05-12 18:05:54 +0800

    feat: 上传 app 成功后会开启异步队列解析 app 的更多数据

--- app/uploaders/app_icon_uploader.rb
+++ app/uploaders/app_icon_uploader.rb
@@ -1,7 +1,7 @@
 class AppIconUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -20,27 +20,28 @@ class AppIconUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  version :thumb do
+    process resize_to_fit: [30, 30]
+  end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
+  version :medium do
+    process resize_to_fit: [80, 80]
+  end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+  version :large do
+    process resize_to_fit: [120, 120]
+  end
+
   def extension_white_list
     %w(png)
   end
 
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def md5
+    chunk = model.send(mounted_as)
+    @md5 ||= Digest::MD5.hexdigest(chunk.read.to_s)
+  end
+
+  def filename
+    @name ||= "#{md5}#{File.extname(super)}" if super
+  end
 end
