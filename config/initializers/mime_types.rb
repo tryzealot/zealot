95fe202e299db5f2e433de66bc682993a05a3e06
icyleaf <icyleaf.cn@gmail.com>
2016-05-26 14:18:34 +0800

    feat: bearychat web hook 返回 app qr code

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,14 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
-# Mime::Type.register "text/richtext", :rtf
+# Mime::Type.register 'text/richtext', :rtf
 
-Mime::Type.register "application/javascript", :pac
+Mime::Type.register 'application/javascript', :pac
 Mime::Type.register 'application/octet-stream', :plist
 
 ActionController::Renderers.add :plist do |data, options|
   data = data.as_json(options) unless options[:skip_serialization] == true
 
   self.content_type ||= Mime::PLIST
-  self.response_body = data.to_plist(:convert_unknown_to_string => true)
+  self.response_body = data.to_plist(convert_unknown_to_string: true)
 end
