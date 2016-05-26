09409f4562a6f4b4815f7ee2a44dd5f7f225ce92
icyleaf <icyleaf.cn@gmail.com>
2016-05-26 16:34:33 +0800

    fix: qrcode 在未登录下无法查看

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -5,10 +5,17 @@
 
 Mime::Type.register 'application/javascript', :pac
 Mime::Type.register 'application/octet-stream', :plist
+Mime::Type.register 'image/png', :png
 
 ActionController::Renderers.add :plist do |data, options|
   data = data.as_json(options) unless options[:skip_serialization] == true
 
   self.content_type ||= Mime::PLIST
   self.response_body = data.to_plist(convert_unknown_to_string: true)
+end
+
+ActionController::Renderers.add :qrcode do |string, options|
+  data = RQRCode::QRCode.new(string).as_png(options)
+  self.content_type ||= Mime::PNG
+  self.response_body = render_to_string(text: data, template: nil)
 end
