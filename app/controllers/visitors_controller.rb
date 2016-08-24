64897d5b687d082ed65cf9d122a19768552c5353
icyleaf <icyleaf.cn@gmail.com>
2016-08-24 16:17:54 +0800

    test

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,10 @@
 class VisitorsController < ApplicationController
-  before_filter :authenticate_user!
+  before_filter :authenticate_user!, only: [:index]
 
   def index
     @title = '我的控制台'
+  end
+
+  def test
   end
 end
