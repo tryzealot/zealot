66ed004d63ad7e1332394eddf72978835dc7ff58
icyleaf <icyleaf.cn@gmail.com>
2016-09-10 09:29:49 +0800

    chroe: 开发环境支持 docker & docker-compose

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,16 @@
+sidekiq_config_url = ENV['JOB_WORKER_URL']
+
+
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: 'redis://localhost:6379',
+    url: sidekiq_config_url,
     namespace: 'qyer:mobile:sidekiq'
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: 'redis://localhost:6379',
+    url: sidekiq_config_url,
     namespace: 'qyer:mobile:sidekiq'
   }
 end
