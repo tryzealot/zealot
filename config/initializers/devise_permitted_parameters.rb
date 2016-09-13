a7a0212e8ef3a684117444f203847ed0e36dd485
icyleaf <icyleaf.cn@gmail.com>
2016-09-13 14:55:25 +0800

    bug: 兼容最新 devise

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,8 +8,8 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :name
-    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up) << :name
+    devise_parameter_sanitizer.permit(:account_update) << :name
   end
 
 end
