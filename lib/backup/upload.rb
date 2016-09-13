cb71fe010fbfeef9d6bdecf815e43345b0788e7a
icyleaf <icyleaf.cn@gmail.com>
2016-09-13 17:54:05 +0800

    change backup upload dir name

--- lib/backup/upload.rb
+++ lib/backup/upload.rb
@@ -11,7 +11,7 @@ module Backup
         Dir.glob("#{app_path}/*").each do |release_path|
           relative_path = release_path.gsub(app_store_path, '')
           app_dirname, release_dirname = relative_path[1..-1].split('/')
-          backup_path = File.join(app_backup_path, app_dirname)
+          backup_path = File.join(uploads_backup_path, app_dirname)
 
           print " * #{app_dirname}/#{release_dirname} ..."
           FileUtils.mkdir_p(backup_path)
@@ -25,9 +25,9 @@ module Backup
     protected
 
     def prepare
-      FileUtils.rm_rf(app_backup_path)
+      FileUtils.rm_rf(uploads_backup_path)
       # Fail if somebody raced to create backup_repos_path before us
-      FileUtils.mkdir_p(app_backup_path, mode: 0700)
+      FileUtils.mkdir_p(uploads_backup_path, mode: 0700)
     end
   end
 end
