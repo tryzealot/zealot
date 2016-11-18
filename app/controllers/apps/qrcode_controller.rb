b866464c2e954ebe1c0ec59a2c69cec8e487a451
icyleaf <icyleaf.cn@gmail.com>
2016-11-18 17:51:00 +0800

    refactor: 抽离 apps/qrcode 模块

---
+++ app/controllers/apps/qrcode_controller.rb
@@ -0,0 +1,34 @@
+class Apps::QrcodeController < AppsController
+  before_action :set_app
+
+  ##
+  # 显示应用的二维码
+  # GET /apps/:slug/(:version)/qrcode
+  def index
+    render qrcode: qrcode_url,
+           module_px_size: qrcode_size,
+           fill: '#F4F5F6',
+           color: '#465960'
+  end
+
+  private
+
+  def qrcode_url
+    if params[:version]
+      url_for(@app, version: @app.releases.last)
+    else
+      url_for(@app)
+    end
+  end
+
+  def qrcode_size
+    case params[:size]
+    when 'medium'
+      4
+    when 'large'
+      6
+    else
+      2
+    end
+  end
+end
