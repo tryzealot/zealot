df9eeec5a50831584e9e4cd261e216931df0b239
icyleaf <icyleaf.cn@gmail.com>
2016-11-28 17:56:01 +0800

    refactor: 重构备份脚本并补充回复脚本

--- lib/backup/database.rb
+++ lib/backup/database.rb
@@ -8,7 +8,7 @@ module Backup
       prepare
 
       compress_rd, compress_wr = IO.pipe
-      compress_pid = spawn(*%W(gzip -1 -c), in: compress_rd, out: [db_file_name, 'w', 0600])
+      compress_pid = spawn(*%W(gzip -1 -c), in: compress_rd, out: [db_backup_file_path, 'w', 0600])
       compress_rd.close
 
       dump_pid = case db_config['adapter']
@@ -24,16 +24,18 @@ module Backup
 
       success = [compress_pid, dump_pid].all? { |pid| Process.waitpid(pid); $?.success? }
       abort 'Backup failed' unless success
+
+      puts '[DONE]'
     end
 
     def restore
       decompress_rd, decompress_wr = IO.pipe
-      decompress_pid = spawn(*%W(gzip -cd), out: decompress_wr, in: db_file_name)
+      decompress_pid = spawn(*%W(gzip -cd), out: decompress_wr, in: db_backup_file_path)
       decompress_wr.close
 
-      restore_pid = case config["adapter"]
+      restore_pid = case db_config["adapter"]
       when /^mysql/ then
-        $progress.print "Restoring MySQL database #{config['database']} ... "
+        print "Restoring MySQL database #{db_config['database']} ... "
         # Workaround warnings from MySQL 5.6 about passwords on cmd line
         ENV['MYSQL_PWD'] = db_config['password'].to_s if db_config['password']
         spawn('mysql', *mysql_args, db_config['database'], in: decompress_rd)
@@ -42,6 +44,8 @@ module Backup
 
       success = [decompress_pid, restore_pid].all? { |pid| Process.waitpid(pid); $?.success? }
       abort 'Restore failed' unless success
+
+      puts '[DONE]'
     end
 
     protected
@@ -57,16 +61,9 @@ module Backup
       args.map { |opt, arg| "#{arg}=#{db_config[opt]}" if db_config[opt] }.compact
     end
 
-    def pg_env
-      ENV['PGUSER']     = db_config['username'] if db_config['username']
-      ENV['PGHOST']     = db_config['host'] if db_config['host']
-      ENV['PGPORT']     = db_config['port'].to_s if db_config['port']
-      ENV['PGPASSWORD'] = db_config['password'].to_s if db_config['password']
-    end
-
     def prepare
       FileUtils.mkdir_p(db_backup_path)
-      FileUtils.rm_f(db_file_name)
+      FileUtils.rm_f(db_backup_file_path)
     end
   end
 end
