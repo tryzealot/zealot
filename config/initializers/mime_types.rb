d57bf85ceeb339db7d7788446547c8848199c5ea
icyleaf <icyleaf.cn@gmail.com>
2016-12-07 11:22:08 +0800

    refactor: 清理 mime)type 已经不用的代码

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,15 +4,6 @@
 # Mime::Type.register "text/richtext", :rtf
 
 Mime::Type.register 'application/javascript', :pac
-# Mime::Type.register 'application/octet-stream', :plist
-# Mime::Type.register 'image/png', :png
-
-# ActionController::Renderers.add :plist do |data, options|
-#   data = data.as_json(options) unless options[:skip_serialization] == true
-#
-#   self.content_type ||= Mime::PLIST
-#   self.response_body = data.to_plist(convert_unknown_to_string: true)
-# end
 
 ActionController::Renderers.add :qrcode do |string, options|
   data = RQRCode::QRCode.new(string).as_png(options)
