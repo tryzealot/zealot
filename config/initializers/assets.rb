680ee72d34509b885b0c1198405c9ac801982589
icyleaf <icyleaf.cn@gmail.com>
2016-12-08 13:28:33 +0800

    chore: 更新 app-info 依赖到 1.0.3

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( maplace.js demo/plans.js )
+# Rails.application.config.assets.precompile += %w( maplace.js demo/plans.js )
 Rails.application.config.assets.precompile += %w( admin-lte/dist/img/* )
