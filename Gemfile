fef2b92d64d29c01b558e621c55218d83c11b0be
icyleaf <icyleaf.cn@gmail.com>
2017-01-12 18:44:19 +0800

    fix: 先删除 spaceship

--- Gemfile
+++ Gemfile
@@ -81,7 +81,7 @@ gem 'uglifier'
 # JS Ace 文本编辑器
 gem 'ace-rails-ap'
 
-gem 'spaceship', require: false
+# gem 'spaceship', require: false
 # 用于解析 ipa 和 apk 包
 gem 'app-info', '~> 1.0.3', require: false
 
--- app/jobs/sync_app_devices_job.rb
+++ app/jobs/sync_app_devices_job.rb
@@ -12,64 +12,64 @@ class SyncAppDevicesJob < ApplicationJob
     @devices = extra['devices']
 
     logger.info "Processing devices (#{@devices.size})"
-    syncing!
+    # syncing!
   end
 
-  def syncing!
-    logger.info 'Syncing devices of release from iDP'
-    accounts.each do |key, account|
-      account_password_variable_name = "#{key.upcase}_PASSWORD"
-      unless ENV.key?(account_password_variable_name)
-        logger.info "No found password of account: #{account_password_variable_name}"
-        next
-      end
-
-      ENV['FASTLANE_PASSWORD'] = ENV[account_password_variable_name]
-      ENV['FASTLANE_TEAM_ID'] = account[:team_id] unless account.key?(:team_id)
-
-      logger.info "[#{account[:user]}] Logining to sync ... "
-      Spaceship.login(account[:user])
-      Spaceship.select_team
-
-      Spaceship::Portal.device.all(include_disabled: true).each do |item|
-        sync_devices.each do |udid|
-          next if udid != item.udid
-
-          params = {
-            name: item.name,
-            udid: item.udid,
-            model: item.model,
-            platform: item.platform,
-            device_type: item.device_type
-          }
-
-          device = Device.find_by(udid: udid)
-          if device
-            device.update!(params)
-          else
-            device = Device.new(params)
-            device.save!
-          end
-
-          sync_devices.delete(udid)
-        end
-      end
-
-      ENV.delete('FASTLANE_PASSWORD')
-      ENV.delete('FASTLANE_TEAM_ID') unless account.key?(:team_id)
-    end
-  end
-
-  def store_devices
-    logger.info 'Store devices of release into the database'
-    @devices.each do |udid|
-      device = Device.find_by(udid: udid)
-      next if device
-
-      device = Device.new(udid: udid)
-      device.save
-    end
-  end
+  # def syncing!
+  #   logger.info 'Syncing devices of release from iDP'
+  #   accounts.each do |key, account|
+  #     account_password_variable_name = "#{key.upcase}_PASSWORD"
+  #     unless ENV.key?(account_password_variable_name)
+  #       logger.info "No found password of account: #{account_password_variable_name}"
+  #       next
+  #     end
+  #
+  #     ENV['FASTLANE_PASSWORD'] = ENV[account_password_variable_name]
+  #     ENV['FASTLANE_TEAM_ID'] = account[:team_id] unless account.key?(:team_id)
+  #
+  #     logger.info "[#{account[:user]}] Logining to sync ... "
+  #     Spaceship.login(account[:user])
+  #     Spaceship.select_team
+  #
+  #     Spaceship::Portal.device.all(include_disabled: true).each do |item|
+  #       sync_devices.each do |udid|
+  #         next if udid != item.udid
+  #
+  #         params = {
+  #           name: item.name,
+  #           udid: item.udid,
+  #           model: item.model,
+  #           platform: item.platform,
+  #           device_type: item.device_type
+  #         }
+  #
+  #         device = Device.find_by(udid: udid)
+  #         if device
+  #           device.update!(params)
+  #         else
+  #           device = Device.new(params)
+  #           device.save!
+  #         end
+  #
+  #         sync_devices.delete(udid)
+  #       end
+  #     end
+  #
+  #     ENV.delete('FASTLANE_PASSWORD')
+  #     ENV.delete('FASTLANE_TEAM_ID') unless account.key?(:team_id)
+  #   end
+  # end
+  #
+  # def store_devices
+  #   logger.info 'Store devices of release into the database'
+  #   @devices.each do |udid|
+  #     device = Device.find_by(udid: udid)
+  #     next if device
+  #
+  #     device = Device.new(udid: udid)
+  #     device.save
+  #   end
+  # end
 
   def sync_devices
     return @sync_devices if @sync_devices
