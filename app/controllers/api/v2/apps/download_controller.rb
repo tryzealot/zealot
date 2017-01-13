84f3b8a535625c4dc2f0d9271aa8d2046ffe03cb
icyleaf <icyleaf.cn@gmail.com>
2017-01-13 22:17:57 +0800

    fix: 获取 install_url 显示 xml 页面的问题

--- app/controllers/api/v2/apps/download_controller.rb
+++ app/controllers/api/v2/apps/download_controller.rb
@@ -1,7 +1,12 @@
 class Api::V2::Apps::DownloadController < ActionController::API
   def show
     @app = App.friendly.find(params[:slug])
-    @release = @app.releases.find_by(version: params[:version])
+    @release =
+      if params[:version]
+        @app.releases.find_by(version: params[:version])
+      else
+        @app.latest_release
+      end
 
     if @app && @release && File.exist?(@release.file.path)
       headers['Content-Length'] = @release.filesize
