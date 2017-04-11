c13112f65f0afb053de76714faf5e6950a0a4fdb
icyleaf <icyleaf.cn@gmail.com>
2017-04-11 17:23:25 +0800

    refactor: bin/background_jobs 调整

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,10 +8,10 @@ set :log_level, :debug
 set :linked_files, %w(config/database.yml)
 set :linked_dirs, %w(log tmp/backups tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads)
 set :keep_releases, 10
-set :default_env, {
-  'JOB_WORKER_URL' => 'redis://localhost:6379/0',
-  'REDIS_URL' => 'redis://localhost:6379/0',
-}
+# set :default_env, {
+#   'JOB_WORKER_URL' => 'redis://localhost:6379/0',
+#   'REDIS_URL' => 'redis://localhost:6379/0',
+# }
 
 # rvm
 set :rvm_type, :user # Defaults to: :auto
