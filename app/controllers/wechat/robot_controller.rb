39835750c3dc2a064377f6360cf35455118e8502
icyleaf <icyleaf.cn@gmail.com>
2017-06-09 14:40:32 +0800

    wechat: 一键预约

--- app/controllers/wechat/robot_controller.rb
+++ app/controllers/wechat/robot_controller.rb
@@ -4,15 +4,36 @@ class Wechat::RobotController < WechatController
   end
 
   def create
-    message = Wechat::Message.factory(request.raw_post)
+    @message = Wechat::Message.factory(request.raw_post)
 
-    reply = Wechat::TextReplyMessage.new
-    reply.FromUserName = message.ToUserName
-    reply.ToUserName   = message.FromUserName
-    reply.Content      = "我还没有那么智能，请耐心等待手动回复 :P"
-
-    logger.info "Respone: #{reply.to_xml}"
+    reply = case @message.Content
+            when '挂', '1', '挂号', '回龙观'
+              booking_link
+            else
+              default_reply
+            end
 
     render plain: reply.to_xml
+  end
+
+  private
+
+  def booking_link
+    reply = Wechat::LinkReplyMessage.new
+    reply.FromUserName = @message.ToUserName
+    reply.ToUserName   = @message.FromUserName
+    reply.Title        = "挂号 | 积水潭医院回龙观"
+    reply.Description  = "一键直达预约挂号页面"
+    reply.Url          = "https://wechat.benmu-health.com/wechat/register/index.html#!/selectResource?firstDeptCode=m_FCK_bd926ff4&firstDeptId=3582&firstDeptName=%E5%A6%87%E4%BA%A7%E7%A7%91&hosCode=H1136112&hosName=%E5%8C%97%E4%BA%AC%E7%A7%AF%E6%B0%B4%E6%BD%AD%E5%8C%BB%E9%99%A2%E5%9B%9E%E9%BE%99%E8%A7%82%E9%99%A2%E5%8C%BA&secondDeptCode=1012&secondDeptId=3532&secondDeptName=%E5%A6%87%E4%BA%A7%E7%A7%91%E9%97%A8%E8%AF%8A%E5%9B%9E%E9%BE%99%E8%A7%82"
+
+    reply
+  end
+
+  def default_reply
+    reply = Wechat::TextReplyMessage.new
+    reply.FromUserName = @message.ToUserName
+    reply.ToUserName   = @message.FromUserName
+    reply.Content      = "我还没有那么智能，请耐心等待手动回复 :P"
+    reply
   end
 end
