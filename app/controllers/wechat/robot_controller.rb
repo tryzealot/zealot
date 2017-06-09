ca4a9b36b342d861ed16b4740b568d9ea6d957e6
icyleaf <icyleaf.cn@gmail.com>
2017-06-09 14:01:50 +0800

    test

--- app/controllers/wechat/robot_controller.rb
+++ app/controllers/wechat/robot_controller.rb
@@ -4,13 +4,13 @@ class Wechat::RobotController < WechatController
   end
 
   def create
-    logger.info params.to_h
-    logger.info request.raw_post
-    message = Wechat::TextReplyMessage.new
-    message.FromUserName = "icyleaf"
-    message.ToUserName   = params[:openid]
-    message.Content      = "hello"
+    message = Wechat::Message.factory(request.raw_post)
 
-    render plain: message.to_xml
+    reply = Wechat::TextReplyMessage.new
+    reply.FromUserName = message.ToUserName
+    reply.ToUserName   = message.FromUserName
+    reply.Content      = "hello"
+
+    render plain: reply.to_xml
   end
 end
