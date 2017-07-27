188418f589a2090382732dd686b758e9dd54c35a
icyleaf <icyleaf.cn@gmail.com>
2017-07-27 19:12:24 +0800

    test

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( admin-lte/dist/img/* )
-Rails.application.config.assets.precompile += %w( font-awesome/font/* )
+Rails.application.config.assets.precompile += %w( font-awesome/fonts/* )
