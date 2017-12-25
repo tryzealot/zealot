f3d28157a382dadae9ae36800e73867965bbec25
icyleaf <icyleaf.cn@gmail.com>
2017-12-25 16:17:54 +0800

    refactor: 更新用户默认的权限数据

--- db/migrate/20171225065144_add_value_to_roles.rb
+++ db/migrate/20171225065144_add_value_to_roles.rb
@@ -1,5 +1,21 @@
 class AddValueToRoles < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :roles, :value, :string, after: :name
+
+    Role.all.each do |role|
+      name, value = case role.name
+                    when 'admin', '管理员'
+                      ['管理员', 'admin']
+                    when 'mobile', '移动开发者'
+                      ['移动开发者', 'mobile']
+                    end
+      if name && value
+        role.update(name: name, value: value)
+      end
+    end
+  end
+
+  def down
+    remove_column :roles, :value
   end
 end
