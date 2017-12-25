6fa89743779abed7f3d042c27bda6c02d9f45975
icyleaf <icyleaf.cn@gmail.com>
2017-12-25 11:58:11 +0800

    feat: 增加用户编辑页面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @title = '用户管理'
@@ -7,7 +8,27 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @roles = Role.all
     redirect_to :back, alert: 'Access denied.' unless @user == current_user || @user.role?(:admin)
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :current_password, :password, :password_confirmation, :roles
+    )
   end
 end
