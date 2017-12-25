1486d08d4e509e72ab54632051dd248c4984dd26
icyleaf <icyleaf.cn@gmail.com>
2017-12-25 11:34:55 +0800

    feat: 增加用户管理列表（仅限于管理员使用)

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,13 @@ Rails.application.routes.draw do
 
   # 用户
   devise_for :users
+  get 'users', to: 'users#index', as: 'users'
+  get 'users/new', to: 'users#new', as: 'new_user'
+  post 'users', to: 'users#create'
+  get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
+  patch 'users/:id', to: 'users#update', as: 'user'
+  put 'users/:id', to: 'users#update'
+  delete 'users/:id', to: 'users#destroy'
 
   authenticate :user do
     require 'sidekiq/web'
