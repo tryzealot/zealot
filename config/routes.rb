94ab9c78c25cc1610ec9516b8914bc8e4a8e449d
icyleaf <icyleaf.cn@gmail.com>
2017-12-26 17:25:44 +0800

    feat: 完成用户激活

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'activations/show'
-  end
-
   namespace :apps, path: 'apps/:slug', slug: /\w+/ do
     get '(:version)/qrcode', to: 'qrcode#show', as: 'qrcode', version: /\d+/
 
@@ -54,8 +50,9 @@ Rails.application.routes.draw do
   patch 'users/:id/update', to: 'users#update', as: 'update_user'
   put 'users/:id/update', to: 'users#update'
   delete 'users/:id', to: 'users#destroy', as: 'user'
-  namespace :users do
-    get 'active/:token', to: 'activations#show', as: 'active_user'
+  scope :users, module: 'users' do
+    get 'active/:token', to: 'activations#edit', as: 'active_user'
+    patch 'active/:token', to: 'activations#update'
   end
 
   authenticate :user do
