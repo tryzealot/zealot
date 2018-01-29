55843a61499f96277e220837f8c3ce7d975f99a6
icyleaf <icyleaf.cn@gmail.com>
2018-01-29 16:38:12 +0800

    refactor: 抽离公共方法到API父类

---
+++ app/controllers/api/base_controller.rb
@@ -0,0 +1,32 @@
+class API::BaseController < ActionController::API
+
+  def validate_user_key
+    @user = User.find_by(key: params[:key])
+
+    raise ActionCable::Connection::Authorization::UnauthorizedError, '未授权用户' unless @user
+  end
+
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActionCable::Connection::Authorization::UnauthorizedError, with: :render_unauthorized_user_key
+  rescue_from ArgumentError, NoMethodError, Mysql2::Error, with: :render_internal_server_error
+
+  def render_unauthorized_user_key(exception)
+    render json: {
+      error: exception.message
+    }, status: :unauthorized
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: {
+      error: 'resource could not be processed with errors',
+      entry: exception.record.errors
+    }, status: :unprocessable_entity
+  end
+
+  def render_internal_server_error(exception)
+    render json: {
+      error: exception.message,
+      entry: exception.backtrace
+    }, status: :internal_server_error
+  end
+end
