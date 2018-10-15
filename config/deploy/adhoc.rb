af9ee1c3285f68a29a1f7afa6ac8bce68c384243
icyleaf <icyleaf.cn@gmail.com>
2018-10-15 17:15:31 +0800

    chore: 227 部署改为 adhoc，新 production 改为 229

---
+++ config/deploy/adhoc.rb
@@ -0,0 +1,47 @@
+set :stage, :production
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# role :app, %w(wangshen@172.1.1.227)
+# role :web, %w(nginx@172.1.1.227)
+# role :db,  %w(wangshen@172.1.1.227)
+# role :puma_nginx, %w(wangshen@172.1.1.227)
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+server '172.1.1.227:2233', user: 'wangshen', roles: %w(web app db puma_nginx puma_role)
+
+# rvm
+set :rvm_type, :user # Defaults to: :auto
+set :rvm_ruby_version, '2.4.1' # Defaults to: 'default'
+
+# you can set custom ssh options
+# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
+# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
+# set it globally
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+# and/or per server
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
+# setting per server overrides global ssh_options
+
+# fetch(:default_env).merge!(rails_env: :production)
