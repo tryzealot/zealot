0fa06827cc5d0a99b17d01b2749c8cea40aaf89e
icyleaf <icyleaf.cn@gmail.com>
2018-10-15 13:39:43 +0800

    chore: 修改 cap 3.5+ 以上 link dirs 的调用方法

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
 set :application, 'mobile'
 set :repo_url, 'git@git.2b6.me:icyleaf/qmobile.git'
 set :branch, 'develop'
 set :deploy_to, '/home/wangshen/www/mobile'
-# set :format, :pretty
 set :log_level, :debug
-
-set :linked_files, %w(config/database.yml)
-set :linked_dirs, %w(log tmp/backups tmp/pids tmp/cache tmp/sockets public/system public/uploads node_modules public/mirrors)
 set :keep_releases, 3
+
+append :linked_files, "config/database.yml"
+append :linked_dirs, "log", "tmp/backups", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "public/mirrors", "node_modules"
+
 # set :default_env, {
 #   'JOB_WORKER_URL' => 'redis://localhost:6379/0',
 #   'REDIS_URL' => 'redis://localhost:6379/0',
@@ -16,14 +19,12 @@ set :keep_releases, 3
 # rvm
 set :rvm_type, :user # Defaults to: :auto
 set :rvm_ruby_version, '2.4.1' # Defaults to: 'default'
-# set :rvm_custom_path, '~/.myveryownrvm'  # only needed if not detected
 
 # bundler
 # set :bundle_flags, '--deployment --quiet -- --use-system-libraries=true'
 set :bundle_env_variables, { NOKOGIRI_USE_SYSTEM_LIBRARIES: 1 }
 
 # nginx
-# set :nginx_use_ssl, true
 set :nginx_server_name, 'mobile.2b6.me'
 set :nginx_sites_available_path, '/home/wangshen/nginx/sites-available'
 set :nginx_sites_enabled_path, '/home/wangshen/nginx/sites-enabled'
@@ -41,5 +42,3 @@ namespace :deploy do
 
   after :finished, 'whenever:update_crontab'
 end
-
-# after 'puma:restart', 'puma:start'
