d8059c6a548db03df233f072084791b4370314a1
icyleaf <icyleaf.cn@gmail.com>
2019-07-08 17:32:30 +0800

    refactor: init user roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Dump default roles
+CreateRolesService.new.call
+
+# Create default admin account
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
