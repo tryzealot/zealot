d2bdf0f356f63ba13d9c366133483ef616450fd6
icyleaf <icyleaf.cn@gmail.com>
2019-08-30 19:13:55 +0800

    fix(api): 修复已存在 app 上传包对于原有数据的更新

--- app/controllers/api/v2/apps/test_controller.rb
+++
@@ -1,9 +0,0 @@
-class Api::V2::Apps::TestController < ActionController::API
-  def show
-    @release = Release.find 16
-
-    render json: @release,
-           serializer: Api::UploadAppSerializer,
-           status: :created
-  end
-end
