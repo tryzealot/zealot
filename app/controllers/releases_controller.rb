ccd50a56dce502380865abab2b60ca7110815533
icyleaf <icyleaf.cn@gmail.com>
2019-09-04 18:48:08 +0800

    refactor: UI 适配调整

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,45 +1,34 @@
 class ReleasesController < ApplicationController
   # before_action :authenticate_user!, except: [:show, :auth]
-  before_action :set_app, only: [:show]
-  # before_action :fetch_apps, only: [:index]
-
-  # ##
-  # # App 列表
-  # # GET /apps
-  # def index
-  #   @title = '应用管理'
-  # end
+  # before_action :set_app, only: [:show]
+  before_action :set_channel, only: [:new, :create]
 
   ##
   # 查看应用详情
-  # GET /apps/:slug
+  # GET /channels/:channel_id/releases/:id
   def show
     @title = @release.app_name
   end
 
-  # ##
-  # # 新应用页面
-  # # GET /apps/new
-  # def new
-  #   @title = '新建应用'
-  #   @app = App.new
-  #   @app.schemes.build
-  # end
+  ##
+  # 上传应用页面
+  # GET /channels/:channel_id/releasess/upload
+  def new
+    @title = '上传应用'
+    @release = @channel.releases.new
+  end
 
-  # ##
-  # # 创建新应用
-  # # POST /apps/create
-  # def create
-  #   schemes = app_params.delete(:schemes_attributes)
-  #   channel = app_params.delete(:channel)
+  ##
+  # 创建新应用
+  # POST /channels/:channel_id/releases/create
+  def create
+    controller = Api::V2::Apps::UploadController.new
+    controller.request = request
+    controller.response = response
+    response = controller.create
 
-  #   # return render json: channels
-  #   @app = App.new(app_params)
-  #   return render :new unless @app.save
-
-  #   create_schemes_by(@app, schemes, channel)
-  #   redirect_to apps_path, notice: '应用已经创建成功！'
-  # end
+    # redirect_to channel_release_path(), notice: '应用已经创建成功！'
+  end
 
   # ##
   # # 编辑应用页面
@@ -83,6 +72,10 @@ class ReleasesController < ApplicationController
   def set_app
     @release = Release.find(params[:id])
     @app = @release.channel.scheme.app
+  end
+
+  def set_channel
+    @channel = Channel.friendly.find params[:channel_id]
   end
 
   # def fetch_apps
