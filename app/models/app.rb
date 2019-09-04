6ecacc9e8137497eae36ebf4391782830f47c43b
icyleaf <icyleaf.cn@gmail.com>
2019-09-04 19:20:02 +0800

    refactor(controller): channel 除了 index 和 show 其他 URI 挂在 app/xxx/schemes/yyy/ 下

--- app/models/app.rb
+++ app/models/app.rb
@@ -6,4 +6,12 @@ class App < ApplicationRecord
   accepts_nested_attributes_for :schemes
 
   validates :name, presence: true
+
+  def recently_release
+    return unless scheme = schemes.take
+    return unless channel = scheme.channels.take
+    return unless release = channel.releases.take
+
+    release
+  end
 end
