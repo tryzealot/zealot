a4fcaefa9be21d2ff696d1a719a0bd70a95e4686
icyleaf <icyleaf.cn@gmail.com>
2019-09-06 12:01:23 +0800

    db: 补充 Android 可用的范例项目

--- app/services/create_sample_apps_service.rb
+++ app/services/create_sample_apps_service.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 class CreateSampleAppsService
-  APP_NAME = '演示应用'
-  APP_BUNDLE_ID = 'com.zealot.app-demo'
-  SCHEMES = %i[开发版 测试版 产品版].freeze
-  CHANNELS = %i[Android iOS].freeze
-  CHANGELOG = "release: 发布 0.1.0\nfix: 修复 xxx 问题\nfeat: 初始化项目"
-
   def call(user)
-    app = App.find_or_create_by name: APP_NAME, user: user
-    SCHEMES.each do |scheme_name|
+    stardford_app user
+    android_channels_app user
+  end
+
+  private
+
+  def stardford_app(user)
+    app_name = '演示应用'
+    app_bundle_id = 'com.zealot.app-demo'
+    schemes = %i[开发版 测试版 产品版].freeze
+    channels = %i[Android iOS].freeze
+    changelog = "release: 发布 0.1.0\nfix: 修复 xxx 问题\nfeat: 初始化项目"
+
+    app = App.find_or_create_by name: app_name, user: user
+    schemes.each do |scheme_name|
       scheme = app.schemes.find_or_create_by name: scheme_name
-      CHANNELS.each do |channel_name|
+      channels.each do |channel_name|
         channel = scheme.channels.find_or_create_by name: channel_name,
                                                     device_type: channel_name.downcase.to_sym
-        bundle_id = generate_bundle_id channel
+        bundle_id = generate_bundle_id app_bundle_id, channel
         release_type = case scheme.name
                        when '开发版'
                          'debug'
@@ -33,16 +40,41 @@ class CreateSampleAppsService
           release.source = 'API'
           release.branch = 'develop'
           release.git_commit = SecureRandom.hex
-          release.changelog = CHANGELOG
+          release.changelog = changelog
           release.save validate: false
         end
       end
     end
   end
 
-  private
+  def android_channels_app(user)
+    app_name = '演示桌面'
+    app_bundle_id = 'com.zealot.android.app-demo'
+    schemes = %i[产品版].freeze
+    channels = %i[华为 小米 oppo viio 魅族 应用宝 百度 GooglePlay].freeze
+    changelog = "release: 发布 0.1.0\nfix: 修复 xxx 问题\nfeat: 初始化项目"
 
-  def generate_bundle_id(channel)
-    "#{APP_BUNDLE_ID}.#{channel.name.downcase}"
+    app = App.find_or_create_by name: app_name, user: user
+    schemes.each do |scheme_name|
+      scheme = app.schemes.find_or_create_by name: scheme_name
+      channels.each do |channel_name|
+        channel = scheme.channels.find_or_create_by name: channel_name,
+                                                    device_type: :android
+        release = channel.releases.new
+        release.bundle_id = app_bundle_id
+        release.release_version = '1.0.0'
+        release.build_version = '1'
+        release.release_type = 'release'
+        release.source = 'API'
+        release.branch = 'develop'
+        release.git_commit = SecureRandom.hex
+        release.changelog = changelog
+        release.save validate: false
+      end
+    end
+  end
+
+  def generate_bundle_id(app_bundle_id, channel)
+    "#{app_bundle_id}.#{channel.name.downcase}"
   end
 end
