ad36fe33c1b3204df388d07bf2d0b7638a7136e7
icyleaf <icyleaf.cn@gmail.com>
2019-09-10 15:49:32 +0800

    refactor(dsym): 修改表结构的字段名

--- db/migrate/20170818033404_create_dsyms.rb
+++ db/migrate/20170818033404_create_dsyms.rb
@@ -2,11 +2,11 @@ class CreateDsyms < ActiveRecord::Migration[6.0]
   def change
     create_table :dsyms do |t|
       t.references :app, index: true, foreign_key: { on_delete: :cascade }
-      t.string :bundle_id
       t.string :release_version
       t.string :build_version
       t.string :file
-      t.string :file_hash
+      t.string :checksum
+
       t.timestamps
     end
   end
