a229100cdd93f3f29cce64cc63efcf216754474a
icyleaf <icyleaf.cn@gmail.com>
2019-10-09 19:19:41 +0800

    refactor: dsym 管理改名为 debug file 管理，支持 Android mapping 文件上传

--- db/migrate/20170818033404_create_debug_files.rb
+++ db/migrate/20170818033404_create_debug_files.rb
@@ -1,6 +1,6 @@
-class CreateDsyms < ActiveRecord::Migration[6.0]
+class CreateDebugFiles < ActiveRecord::Migration[6.0]
   def change
-    create_table :dsyms do |t|
+    create_table :debug_files do |t|
       t.references :app, index: true, foreign_key: { on_delete: :cascade }
       t.string :release_version
       t.string :build_version
