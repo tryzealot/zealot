a89d4608a6e5b76bf9dea2a3c1580e4c8a2379ff
icyleaf <icyleaf.cn@gmail.com>
2019-10-17 17:43:13 +0800

    refactor(debug_file): 拆分 ios 和 android

--- app/assets/javascripts/debug_file.js
+++ app/assets/javascripts/debug_file.js
@@ -1,9 +1,6 @@
 $(document).on('turbolinks:load', function () {
-  $('.debug-file-section').click(function () {
+  $('.debug-file-toggle').click(function () {
     debug_file_id = $(this).data('id');
-
-    console.info(debug_file_id);
-    console.info($('#debug-file-metadata-' + debug_file_id).html());
     $('#debug-file-metadata-' + debug_file_id).toggleClass('hidden');
   })
 });
