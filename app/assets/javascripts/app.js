f80eeddd8f332908929595684e23fa8bd83edc17
icyleaf <icyleaf.cn@gmail.com>
2019-10-17 16:26:23 +0800

    fix: 只在系统信息页面检查版本号

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -68,11 +68,6 @@ function check_new_version() {
   }
 }
 
-$('#latest_version').ready(function () {
-  check_new_version();
-});
-
-
 function sleep(ms) {
   var start = new Date().getTime();
   while (new Date().getTime() - start < ms)
@@ -149,103 +144,8 @@ function hideCover() {
   $('.navbar-fixed-top').css('z-index', 1030);
 }
 
-// function getAverageRGB(imgEl) {
-//   var blockSize = 5, // only visit every 5 pixels
-//       defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-//       canvas = document.createElement('canvas'),
-//       context = canvas.getContext && canvas.getContext('2d'),
-//       data, width, height,
-//       i = -4,
-//       length,
-//       rgb = {r:0,g:0,b:0},
-//       count = 0;
-
-//   if (!context) {
-//       return defaultRGB;
-//   }
-
-//   height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
-//   width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
-
-//   context.drawImage(imgEl, 0, 0);
-
-//   try {
-//       data = context.getImageData(0, 0, width, height);
-//   } catch(e) {
-//       /* security error, img on diff domain */alert('x');
-//       return defaultRGB;
-//   }
-
-//   length = data.data.length;
-
-//   while ( (i += blockSize * 4) < length ) {
-//       ++count;
-//       rgb.r += data.data[i];
-//       rgb.g += data.data[i+1];
-//       rgb.b += data.data[i+2];
-//   }
-
-//   // ~~ used to floor values
-//   rgb.r = ~~(rgb.r/count);
-//   rgb.g = ~~(rgb.g/count);
-//   rgb.b = ~~(rgb.b/count);
-
-//   return rgb;
-// }
-
-// // # bind function
-// // window.download = download();
-// // window.build = build();
-// // window.hideCover = hideCover();
-
-//   // ready = ->
-//   //   Dropzone.options.dropzone =
-//   //     paramName: "file"
-//   //     maxFilesize: 100
-//   //     maxFiles: 1
-//   //     init: ->
-//   //       console.log 'initial'
-//   //     accept: (file, done) ->
-//   //       console.log "uploaded"
-//   //       ext = file.name.split('.').pop().toLowerCase()
-//   //       if $.inArray(ext, ['ipa', 'apk']) == -1 then done("请上传 ipa 或 apk 文件") else done()
-//   //     success: (file, data) ->
-//   //       url = HOST + "releases/" + data.id + "/edit"
-//   //       console.log 'success, redirect to %s', url
-
-// function badget_scm_info() {
-//   var value
-//   $('.git-branch').click(function () {
-//     value = $(this).html();
-//   })
-
-//   var branch = $(this).data('branch')
-//   var commit = $(this).data('commit')
-//   if (value == branch)
-//     $(this).html(commit)
-//   else
-//     $(this).html(branch)
-// }
-
-// $(document).on("turbolinks:load", function () {
-//   //  if window.location.pathname == '/apps/upload'
-//   //  ready
-//   //  else
-//   //  document.addEventListener('turbolinks:load', badget_scm_info)
-
-//   $('.release-changelogs-toggle').click(function () {
-//     var release_id = $(this).data('id');
-//     $('.release-' + release_id + '-changelog').toggleClass('hide');
-//   })
-// });
-
-
-// // $(document).ready(function () {
-// //   $('#apps-list .app').each(function (index) {
-// //     var icon = $(this).find('.app-icon').first();
-// //     console.log(icon);
-// //     var rgb = getAverageRGB(icon[0]);
-// //     console.log(rgb);
-// //     $(this).find('.info-box').css('backgroundColor', 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')');
-// //   })
-// // });
+$('#latest_version').ready(function () {
+  if (window.location.pathname == '/admin/system_info') {
+    check_new_version();
+  }
+});
