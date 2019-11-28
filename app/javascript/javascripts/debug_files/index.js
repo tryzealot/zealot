fc48b3b3e0a88efc92ab1745188320c080ea76cb
icyleaf <icyleaf.cn@gmail.com>
2019-11-28 19:09:45 +0800

    style: 全部 UI 迁移到 AdminLTE3

--- app/javascript/javascripts/debug_files/index.js
+++ app/javascript/javascripts/debug_files/index.js
@@ -1,7 +1,7 @@
 $(document).on('turbolinks:load', function () {
   $('.debug-file-toggle').click(function () {
     debug_file_id = $(this).data('id');
-    $('#debug-file-metadata-' + debug_file_id).toggleClass('hidden');
+    $('#debug-file-metadata-' + debug_file_id).toggleClass('d-none');
   });
 
   $('.destroy-debug-file').click(function () {
