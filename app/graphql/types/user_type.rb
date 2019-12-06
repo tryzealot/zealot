3ac35636549234eba9278cf787aa0d1875062d97
icyleaf <icyleaf.cn@gmail.com>
2019-12-06 16:47:18 +0800

    refactor: 更新 graphql 模板文件同时传入 current_user 全局变量

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module Types
   class UserType < Types::BaseObject
     field :id, Int, null: false
     field :name, String, null: true
     field :email, String, null: false
-    field :key, String, null: false
+    field :token, String, null: false
   end
 end
--- app/graphql/zealot_schema.rb
+++ app/graphql/zealot_schema.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ZealotSchema < GraphQL::Schema
-  # mutation(Types::MutationType)
-  query QueryType
+  mutation(Types::MutationType)
+  query(Types::QueryType)
 end
