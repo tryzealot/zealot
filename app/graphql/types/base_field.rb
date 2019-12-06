69bfaedcbe89397c701306be354d36938dc3ab4f
icyleaf <icyleaf.cn@gmail.com>
2019-12-06 16:47:18 +0800

    refactor: 更新 graphql 模板文件同时传入 current_user 全局变量

--- app/graphql/types/base_field.rb
+++ app/graphql/types/base_field.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
+
 module Types
   class BaseField < GraphQL::Schema::Field
-    def resolve_field(obj, args, ctx)
-      resolve(obj, args, ctx)
-    end
+    argument_class Types::BaseArgument
   end
 end
--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 module Types
   module BaseInterface
     include GraphQL::Schema::Interface
+
+    field_class Types::BaseField
   end
 end
