aabd03df898f07ae0b98ce5b488241269abb5af4
icyleaf <icyleaf.cn@gmail.com>
2019-12-12 15:49:37 +0800

    chore: 开发模式启用 debug 日志级别输出

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,10 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
+  config.log_level = :debug
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
