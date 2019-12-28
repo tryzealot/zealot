a0c7f0cda27f45926faf236f4d24db3ad1d6a8d2
icyleaf <icyleaf.cn@gmail.com>
2019-12-28 14:02:29 +0800

    feat: 管理界面增加查看全局网络钩子

---
+++ app/controllers/admin/web_hooks_controller.rb
@@ -0,0 +1,55 @@
+class Admin::WebHooksController < ApplicationController
+  before_action :set_web_hook, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @title = '网络钩子管理'
+    @web_hooks =  WebHook.all
+    authorize @web_hooks
+  end
+
+  def show
+    @title = '网络钩子详情'
+  end
+
+  def new
+    @title = '新建网络钩子'
+    @web_hook = WebHook.new
+    authorize @web_hook
+  end
+
+  def create
+    @web_hook = WebHook.new(web_hook_params)
+    authorize @web_hook
+
+    return render :new unless @web_hook.save
+
+    redirect_to admin_users_url, notice: '网络钩子创建成功'
+  end
+
+  def edit
+    @title = '编辑网络钩子'
+  end
+
+  def update
+    return render :edit unless @web_hook.update(web_hook_params)
+
+    redirect_to admin_web_hooks_url, notice: '网络钩子已经更新'
+  end
+
+  def destroy
+    @web_hook.destroy
+    redirect_to admin_web_hooks_url, notice: '网络钩子已经删除'
+  end
+
+  private
+
+  def set_web_hook
+    @web_hook = WebHook.find(params[:id])
+    authorize @web_hook
+  end
+
+  def web_hook_params
+    params.require(:web_hook).permit(:url, :upload_events, :download_events, :changelog_events)
+  end
+
+end
