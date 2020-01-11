ac4fc95d3de9d0f15ccd86fbbd825d15023e75d2
icyleaf <icyleaf.cn@gmail.com>
2020-01-11 16:15:16 +0800

    chore: 拆分构建 docker 镜像构建并减少体积

--- docker-endpoint.sh
+++ docker-endpoint.sh
@@ -11,9 +11,9 @@ ZEALOT_READY_FILE=/app/zealot.ready
 if [ ! -f "$ZEALOT_READY_FILE" ]; then
   echo "Waiting zealot to be ready please ... tea time"
   # Init database
-  rails db:create
-  rails db:migrate
-  rails db:seed
+  bin/rails db:create
+  bin/rails db:migrate
+  bin/rails db:seed
 
   # Update cron jobs
   bundle exec whenever --update-crontab
