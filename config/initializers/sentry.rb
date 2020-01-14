88913664599a0832d9cc647d7c21f0efc2c5a47f
icyleaf <icyleaf.cn@gmail.com>
2020-01-14 20:31:55 +0800

    feat: sentry 静默运行

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,6 +3,7 @@
 # 默认开启 Sentry，如果不想使用设置 ZEALOT_SENTRY_DISABLE=1
 if ENV['ZEALOT_SENTRY_DISABLE'].blank?
   Raven.configure do |config|
+    config.silence_ready = true
     config.dsn = ENV['ZEALOT_SENTRY_DNS'] || 'https://133aefa9f52448a1a7900ba9d02f93e1@sentry.io/1878137'
     config.excluded_exceptions += ['ActionController::RoutingError', 'ActiveRecord::RecordNotFound']
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
