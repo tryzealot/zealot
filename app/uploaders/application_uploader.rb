9068225f1418ad5d400930fd5f6ed533d1a52161
icyleaf <icyleaf.cn@gmail.com>
2020-01-16 17:09:06 +0800

    fix(Web): 删除应用和调试文件把生成的结构目录一同删除

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -2,6 +2,11 @@
 
 class ApplicationUploader < CarrierWave::Uploader::Base
   storage :file
+  after :remove, :delete_empty_upstream_dirs
+
+  def base_store_dir
+    'uploads'
+  end
 
   def size
     @size = file&.size
@@ -14,5 +19,18 @@ class ApplicationUploader < CarrierWave::Uploader::Base
 
   def filename
     @name ||= "#{checksum}#{File.extname(super)}" if super
+  end
+
+  protected
+
+  # Copy from https://github.com/carrierwaveuploader/carrierwave/wiki/how-to:-make-a-fast-lookup-able-storage-directory-structure
+  def delete_empty_upstream_dirs
+    path = ::File.expand_path(store_dir, root)
+    Dir.delete(path) # fails if path not empty dir
+
+    path = ::File.expand_path(base_store_dir, root)
+    Dir.delete(path) # fails if path not empty dir
+  rescue SystemCallError
+    true # nothing, the dir is not empty
   end
 end
