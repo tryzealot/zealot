e8d60812113b2039bd671f6c3ab4696f59d4475b
icyleaf <icyleaf.cn@gmail.com>
2020-05-21 16:21:22 +0800

    feat: 增加 clipboard.js

--- app/controllers/udid_controller.rb
+++ app/controllers/udid_controller.rb
@@ -9,7 +9,7 @@ class UdidController < ApplicationController
     store = OpenSSL::X509::Store.new
     p7sign.verify(nil, store, nil, OpenSSL::PKCS7::NOVERIFY)
 
-    attrs = Plist::parse_xml(p7sign.data)
+    attrs = Plist.parse_xml(p7sign.data)
 
     redirect_to udid_result_url(
       udid: attrs['UDID'],
@@ -26,20 +26,19 @@ class UdidController < ApplicationController
     @udid = Rails.cache.fetch('ios-udid', expires_in: 1.week) { SecureRandom.uuid.upcase }
 
     content_type = params[:preview].present? ? 'html/text' : 'application/x-apple-aspen-config'
+    render content_type: content_type, layout: false
 
-    plist = render_to_string(layout: false)
+    # plist = render_to_string(layout: false)
 
-    server = File.read('public/certs/server.pem')
-    server_key = File.read('public/certs/key.pem')
+    # server = File.read('public/certs/server.pem')
+    # server_key = File.read('public/certs/key.pem')
+    # server_pem = OpenSSL::X509::Certificate.new(server)
+    # server_key_pem = OpenSSL::PKey::RSA.new(server_key)
 
-    server_pem = OpenSSL::X509::Certificate.new(server)
-    server_key_pem = OpenSSL::PKey::RSA.new(server_key)
-
-    sigend_plist = OpenSSL::PKCS7.sign(
-      server_pem, server_key_pem,
-      plist, [], OpenSSL::PKCS7::BINARY
-    )
-
-    render plain: plist, content_type: content_type, layout: false
+    # sigend_plist = OpenSSL::PKCS7.sign(
+    #   server_pem, server_key_pem,
+    #   plist, [], OpenSSL::PKCS7::BINARY
+    # )
+    # render plain: plist, content_type: content_type, layout: false
   end
 end
