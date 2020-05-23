ce27c0cb2df0cecbdcb1eb1a68e668e3b61272d2
icyleaf <icyleaf.cn@gmail.com>
2020-05-23 14:58:57 +0800

    feat: 支持解析已上传应用的包

--- app/controllers/teardowns_controller.rb
+++ app/controllers/teardowns_controller.rb
@@ -1,21 +1,50 @@
 # frozen_string_literal: true
 
 class TeardownsController < ApplicationController
-  # GET /teardowns/
+
+  def index
+    redirect_to new_teardown_path, alert: "链接失效，请重新解析文件"
+  end
+
   def show
   end
 
-  # GET /teardowns/upload
   def new
     @title = '应用解析'
   end
 
-  # POST /teardowns
   def create
+    if params[:file]
+      parse_file
+    else
+      parse_exists_app
+    end
+
+  rescue ActionController::RoutingError => e
+    flash[:error] = e.message
+    render :new
+  rescue AppInfo::UnkownFileTypeError
+    flash[:error] = '无法识别上传的应用类型'
+    render :new
+  rescue AppInfo::NotFoundError => e
+    flash[:error] = "无法找到安装包: #{e}"
+    render :new
+  end
+
+  private
+
+  def parse_file
     file = params[:file]
     @app_info = AppInfo.parse(file.tempfile)
-  rescue AppInfo::UnkownFileTypeError
-    flash.now[:message] = '无法识别上传的应用类型'
-    render :new
+  end
+
+  def parse_exists_app
+    data = Rails.application.routes.recognize_path(params[:url])
+    unless data[:controller] == 'releases' && data[:action] == 'show'
+      raise ActionController::RoutingError, '不是正确的版本详情链接，请重试'
+    end
+
+    release = Release.find(data[:id])
+    @app_info = AppInfo.parse(release.file.file.path)
   end
 end
