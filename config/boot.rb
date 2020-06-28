9f9989002c5e781c6cd65f73b4216a0e5660ddc6
icyleaf <icyleaf.cn@gmail.com>
2020-06-28 18:44:51 +0800

    chore: 使用 s6-overlay 作为多进程管理服务

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,9 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# Broken in docker image, need set it false
+# Ref: https://github.com/Shopify/bootsnap/issues/262
+if ENV.fetch('ENABLE_BOOTSNAP', 'true') == 'true'
+  require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+end
