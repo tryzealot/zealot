7e60dd885af2251af42c62cdd763e458f1a97220
icyleaf <icyleaf.cn@gmail.com>
2020-07-20 18:45:28 +0800

    feat: 配置支持动态保存且管理员支持部分数据修改

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,15 +1,35 @@
+# frozen_string_literal: true
+
 class Admin::SettingsController < ApplicationController
-  # before_action :get_setting, only: [:edit, :update]
+  before_action :set_setting, only: %i[edit update]
 
   def index
     @title = '系统配置'
     @settings = Setting.site_configs
   end
 
+  def edit
+    @title = '编辑设置'
+  end
+
+  def update
+    if @setting.value != setting_param[:value]
+      @setting.value = setting_param[:value]
+      @setting.save
+      redirect_to admin_settings_path, notice: "保存成功."
+    else
+      redirect_to :edit
+    end
+  end
+
   private
 
-    def setting_params
-      params.require(:setting).permit(:host, :user_limits, :admin_emails,
-        :captcha_enable, :notification_options)
+
+    def set_setting
+      @setting = Setting.find_or_default(var: params[:id])
+    end
+
+    def setting_param
+      params[:setting].permit!
     end
 end
