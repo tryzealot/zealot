2256d48ffcc3c5a7c1be38ead2641cf80126a31d
icyleaf <icyleaf.cn@gmail.com>
2020-07-24 11:00:43 +0800

    fix: 解决 Initialization autoloaded the constant Setting 问题

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,27 +2,29 @@
 
 # 默认开启 Sentry，如果不想使用设置 ZEALOT_SENTRY_DISABLE=1
 if ENV['ZEALOT_SENTRY_DISABLE'].blank?
-  Raven.configure do |config|
-    config.silence_ready = true
-    config.dsn = ENV['ZEALOT_SENTRY_DNS'] || 'https://133aefa9f52448a1a7900ba9d02f93e1@sentry.io/1878137'
-    config.excluded_exceptions += [
-      'ActionController::RoutingError',
-      'ActiveRecord::RecordNotFound',
-      'ActiveRecord::RecordInvalid'
-    ]
-    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-    config.sanitize_fields << 'token'
+  Rails.configuration.to_prepare do
+    Raven.configure do |config|
+      config.silence_ready = true
+      config.dsn = ENV['ZEALOT_SENTRY_DNS'] || 'https://133aefa9f52448a1a7900ba9d02f93e1@sentry.io/1878137'
+      config.excluded_exceptions += [
+        'ActionController::RoutingError',
+        'ActiveRecord::RecordNotFound',
+        'ActiveRecord::RecordInvalid'
+      ]
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+      config.sanitize_fields << 'token'
 
-    version = Setting.version
-    vcs_ref = Setting.vcs_ref
-    version = "#{version}-#{vcs_ref}" if vcs_ref.present?
-    config.release = version
+      version = Setting.version
+      vcs_ref = Setting.vcs_ref
+      version = "#{version}-#{vcs_ref}" if vcs_ref.present?
+      config.release = version
 
-    if vcs_ref.present?
-      config.tags = {
-        docker: true,
-        docker_tag: ENV['DOCKER_TAG']
-      }
+      if vcs_ref.present?
+        config.tags = {
+          docker: true,
+          docker_tag: ENV['DOCKER_TAG']
+        }
+      end
     end
   end
 end
