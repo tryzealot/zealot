2a8bdb23ef6ad0b5c488489b77d9c1eebf98d572
icyleaf <icyleaf.cn@gmail.com>
2020-07-24 11:02:26 +0800

    refactor: 显性设置异步任务是 sidekiq

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,14 +66,13 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
 
-  # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "zealot_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default_url_options = { host: Setting.site_domain }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
