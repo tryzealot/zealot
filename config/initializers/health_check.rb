38c2bda2188bc6a55a3af9bf7d4cf1e17a18c5a7
icyleaf <icyleaf.cn@gmail.com>
2020-10-30 15:45:36 +0800

    refactor: 移除 health check 的 ip 白名单

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -6,8 +6,7 @@ HealthCheck.setup do |config|
   config.standard_checks = %w[database migrations cache redis]
   config.full_checks = %w[database migrations cache redis sidekiq-redis]
 
-  ip_whitelist = ENV.fetch('ZEALOT_HEALTH_CHECK_IP_WHITELIST') { '127.0.0.1' }
-  ip_whitelist = ip_whitelist.split(',').map(&:strip)
-
-  config.origin_ip_whitelist = ip_whitelist
+  ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST']
+  ip_whitelist = ip_whitelist.split(',').select(&:present?).map(&:strip) if ip_whitelist.present?
+  config.origin_ip_whitelist = ip_whitelist if ip_whitelist.present? && ip_whitelist.size > 0
 end
