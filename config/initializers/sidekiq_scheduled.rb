954701624842817be51fb6f3e37ba56eb1d06ccf
icyleaf <icyleaf.cn@gmail.com>
2021-01-26 15:57:13 +0800

    fix: 去除 Rails 6 的预加载警告

--- config/initializers/sidekiq_scheduled.rb
+++ config/initializers/sidekiq_scheduled.rb
@@ -13,19 +13,21 @@ SCHEDULED_JOBS = {
   }
 }
 
-if Sidekiq.server?
-  cron_jobs = {}
-  if Setting.keep_uploads
-    Sidekiq::Cron::Job.destroy('clean_old_releases')
-  else
-    cron_jobs[:clean_old_releases] = SCHEDULED_JOBS[:clean_old_releases]
-  end
+Rails.application.reloader.to_prepare do
+  if Sidekiq.server?
+    cron_jobs = {}
+    if Setting.keep_uploads
+      Sidekiq::Cron::Job.destroy('clean_old_releases')
+    else
+      cron_jobs[:clean_old_releases] = SCHEDULED_JOBS[:clean_old_releases]
+    end
 
-  if Setting.demo_mode
-    cron_jobs[:reset_for_demo_mode] = SCHEDULED_JOBS[:reset_for_demo_mode]
-  else
-    Sidekiq::Cron::Job.destroy('reset_for_demo_mode')
-  end
+    if Setting.demo_mode
+      cron_jobs[:reset_for_demo_mode] = SCHEDULED_JOBS[:reset_for_demo_mode]
+    else
+      Sidekiq::Cron::Job.destroy('reset_for_demo_mode')
+    end
 
-  Sidekiq::Cron::Job.load_from_hash cron_jobs
+    Sidekiq::Cron::Job.load_from_hash cron_jobs
+  end
 end
