b98ffa1f49b35e93467c71375c88244029f3e7e2
icyleaf <icyleaf.cn@gmail.com>
2021-07-23 18:07:29 +0800

    fix: 修复使用第三方登录时用户已注册但未验证造成无法登录

--- app/models/user_provider.rb
+++ app/models/user_provider.rb
@@ -3,26 +3,23 @@
 class UserProvider < ApplicationRecord
   belongs_to :user
 
-  def self.initialize_from_omniauth(auth)
-    UserProvider.where(name: auth.provider, uid: auth.uid).first_or_initialize do |provider|
-      credentials = auth.credentials
-
-      provider.token = credentials.token
-      provider.expires = credentials.expires
-      provider.expires_at = credentials.expires_at
-      provider.refresh_token = credentials.refresh_token
-    end
+  def self.from_omniauth(auth)
+    credentials = auth.credentials
+    provider = find_or_create_by(name: auth.provider, uid: auth.uid)
+    provider.update!(
+      token: credentials.token,
+      expires: credentials.expires,
+      expires_at: credentials.expires_at,
+      refresh_token: credentials.refresh_token
+    )
   end
 
-  def self.create_from_omniauth(auth, user)
-    UserProvider.where(name: auth.provider, uid: auth.uid).first_or_create do |provider|
-      credentials = auth.credentials
-
-      provider.user = user
-      provider.token = credentials.token
-      provider.expires = credentials.expires
-      provider.expires_at = credentials.expires_at
-      provider.refresh_token = credentials.refresh_token
-    end
+  def update_omniauth(credentials)
+    update(
+      token: credentials.token,
+      expires: credentials.expires,
+      expires_at: credentials.expires_at,
+      refresh_token: credentials.refresh_token
+    )
   end
 end
