9f49ba5505fe5b6bf224598addd90333185749da
icyleaf <icyleaf.cn@gmail.com>
2021-08-11 18:06:14 +0800

    clean: 移除无用代码

--- app/inputs/rich_input.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class RichInput < SimpleForm::Inputs::Base
-  def input(wrapper_options = nil)
-    merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
-
-    @builder.rich_text_area(attribute_name, merged_input_options)
-  end
-end
