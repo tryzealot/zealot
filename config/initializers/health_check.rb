27dd3aa8dcbb210140c6b31649cf0dbe7afb2801
icyleaf <icyleaf.cn@gmail.com>
2021-08-24 15:58:22 +0800

    refactor: 设置检查 healthly 默认 ip 白名单

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -6,7 +6,7 @@ HealthCheck.setup do |config|
   config.standard_checks = %w[database migrations cache]
   config.full_checks = %w[database migrations cache sidekiq-redis]
 
-  ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST']
+  ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST'] || '127.0.0.1'
   ip_whitelist = ip_whitelist.split(',').select(&:present?).map(&:strip) if ip_whitelist.present?
   config.origin_ip_whitelist = ip_whitelist if ip_whitelist.present? && ip_whitelist.size > 0
 end
