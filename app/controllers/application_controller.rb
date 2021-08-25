1761b15ca0393f9a88dd1a9225875e400caeee8d
icyleaf <icyleaf.cn@gmail.com>
2021-08-25 14:25:35 +0800

    fix: web 的保存需要明确状态码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,35 +43,35 @@ class ApplicationController < ActionController::Base
   end
 
   def forbidden(e)
-    respond_with_error(:forbidden, e)
+    respond_with_error(403, e)
   end
 
   def not_found(e)
-    respond_with_error(:not_found, e)
+    respond_with_error(404, e)
   end
 
   def gone(e)
-    respond_with_error(:gone, e)
+    respond_with_error(410, e)
   end
 
   def unprocessable_entity(e)
-    respond_with_error(:unprocessable_entity, e)
+    respond_with_error(422, e)
   end
 
   def not_acceptable(e)
-    respond_with_error(:not_acceptable, e)
+    respond_with_error(406, e)
   end
 
   def bad_request(e)
-    respond_with_error(:bad_request, e)
+    respond_with_error(400, e)
   end
 
   def internal_server_error(e)
-    respond_with_error(:internal_server_error, e)
+    respond_with_error(500, e)
   end
 
   def service_unavailable(e)
-    respond_with_error(:service_unavailable, e)
+    respond_with_error(503, e)
   end
 
   def respond_with_error(code, exception)
