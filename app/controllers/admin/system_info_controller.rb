b15d0837d11d7d52ca407b654a8cacd23b85dc34
icyleaf <icyleaf.cn@gmail.com>
2021-08-30 18:38:57 +0800

    feat: 磁盘信息增加进度图并修正控制台的值

--- app/controllers/admin/system_info_controller.rb
+++ app/controllers/admin/system_info_controller.rb
@@ -6,8 +6,9 @@ class Admin::SystemInfoController < ApplicationController
   EXCLUDED_MOUNT_OPTIONS = [
     'nobrowse',
     'read-only',
-    'ro'
-  ]
+    'ro',
+    'noexec'
+  ].freeze
 
   EXCLUDED_MOUNT_TYPES = [
     'autofs',
@@ -31,7 +32,7 @@ class Admin::SystemInfoController < ApplicationController
     'tmpfs',
     'tracefs',
     'vfat'
-  ]
+  ].freeze
 
   # GET /admin/system_info
   def index
@@ -41,6 +42,7 @@ class Admin::SystemInfoController < ApplicationController
 
     set_cpus
     set_memory
+    set_diskspace
     set_disks
     set_env
   end
@@ -54,7 +56,14 @@ class Admin::SystemInfoController < ApplicationController
   end
 
   def set_memory
-    @memory = Vmstat.memory
+    memory = Vmstat.memory
+    percent = percent(memory.active_bytes, memory.total_bytes)
+    @memory = {
+      used: memory.active_bytes,
+      total: memory.total_bytes,
+      percent: percent,
+      color: progress_color(percent)
+    }
   rescue
     @memory = nil
   end
@@ -65,11 +74,22 @@ class Admin::SystemInfoController < ApplicationController
     end.sort
   end
 
-  def set_disks
-    mounts = ::Sys::Filesystem.mounts
-    @disks = mounts.each_with_object([]) do |mount, obj|
-      mount_options = mount.options.split(',')
+  def set_diskspace
+    disk = Sys::Filesystem.stat(Rails.root)
+    percent = percent(disk.bytes_used, disk.bytes_total)
+    @diskspace = {
+      used: disk.bytes_used,
+      total: disk.bytes_total,
+      percent: percent,
+      color: progress_color(percent)
+    }
+  rescue
+    @diskspace = nil
+  end
 
+  def set_disks
+    @disks = ::Sys::Filesystem.mounts.each_with_object([]) do |mount, obj|
+      mount_options = mount.options.split(',').map(&:strip)
       next if (EXCLUDED_MOUNT_OPTIONS & mount_options).any?
       next if (EXCLUDED_MOUNT_TYPES & [mount.mount_type]).any?
 
@@ -77,11 +97,13 @@ class Admin::SystemInfoController < ApplicationController
         disk = Sys::Filesystem.stat(mount.mount_point)
         next if obj.any? { |i| i[:mount_path] == disk.path }
 
+        percent = percent(disk.bytes_used, disk.bytes_total)
         obj.push(
           bytes_total: disk.bytes_total,
           bytes_used: disk.bytes_used,
-          disk_name: mount.name,
-          mount_path: disk.path
+          mount_path: disk.path,
+          percent: percent,
+          color: progress_color(percent)
         )
       rescue Sys::Filesystem::Error
         next
@@ -107,5 +129,20 @@ class Admin::SystemInfoController < ApplicationController
     hidden = '*' * (average * 2)
     suffix = chars[(prefix.size + average * 2)..-1]
     "#{prefix}#{hidden}#{suffix}"
+  end
+
+  def percent(value, n)
+    value.to_f / n.to_f * 100.0
+  end
+
+  def progress_color(percent)
+    case percent.to_i
+    when 0..60
+      'bg-success'
+    when 61..80
+      'bg-warning'
+    else
+      'bg-danger'
+    end
   end
 end
