e5ad0bda2a398f8adb9e0de62237a18ad711080b
icyleaf <icyleaf.cn@gmail.com>
2021-09-13 20:21:15 +0800

    feat(i18n): schemes, channels 100%

--- app/controllers/schemes_controller.rb
+++ app/controllers/schemes_controller.rb
@@ -3,52 +3,40 @@
 class SchemesController < ApplicationController
   before_action :authenticate_user!, except: :show
   before_action :set_scheme, except: %i[create new]
+  before_action :set_channel, only: %i[show]
   before_action :set_app
 
   def show
-    if (referer = request.referer) &&
-       (segment = Rails.application.routes.recognize_path(referer)) &&
-       segment[:controller] == 'channels' &&
-       segment[:action] == 'show'
-      previouse_channel = Channel.friendly.find(segment[:id])
-      @channel = @scheme.channels.find_by(device_type:  previouse_channel.device_type)
-    else
-      @channel = @scheme.latest_channel
-    end
-
     if @channel
       redirect_to channel_path(@channel)
     else
-      redirect_to new_app_scheme_channel_path(@scheme.app, @scheme), alert: '没有发现已创建的渠道，请新建一个'
+      redirect_to new_app_scheme_channel_path(@scheme.app, @scheme), alert: t('schemes.show.empty_channel')
     end
   end
 
   def new
-    @title = "新建#{@app.name}类型"
+    @title = t('schemes.new.title', app: @app.name)
     @scheme = Scheme.new
     authorize @scheme
   end
 
   def create
-    @channel = scheme_params.delete(:channel)
+    channels = scheme_params.delete(:channel_attributes)[:name].reject(&:empty?)
     @scheme = Scheme.new(scheme_params)
     authorize @scheme
 
     @scheme.app = @app
     return render :new unless @scheme.save
 
-    create_channel_by(@scheme, @channel)
-    redirect_to app_path(@app), notice: '类型已经创建成功！'
+    create_channels(channels)
+    redirect_to app_path(@app), notice: t('activerecord.success.create', key: t('schemes.title'))
   end
 
   def edit
-    @title = "编辑#{@app.name}类型"
-    raise ActionController::RoutingError, '这里没有你找的东西' unless @scheme
+    @title = t('schemes.edit.title', app: @app.name)
   end
 
   def update
-    raise ActionController::RoutingError, '这里没有你找的东西' unless @scheme
-
     @scheme.update(scheme_params)
     redirect_to app_path(@app)
   end
@@ -61,19 +49,11 @@ class SchemesController < ApplicationController
 
   protected
 
-  def create_channel_by(scheme, channel)
-    return unless channels = channel_value(channel)
+  def create_channels(channels)
+    return if channels.empty?
 
     channels.each do |channel_name|
-      scheme.channels.create name: channel_name, device_type: channel_name.downcase.to_sym
-    end
-  end
-
-  def channel_value(platform)
-    case platform
-    when 'ios' then ['iOS']
-    when 'android' then ['Android']
-    when 'both' then ['Android', 'iOS']
+      @scheme.channels.create name: channel_name, device_type: channel_name.downcase.to_sym
     end
   end
 
@@ -88,6 +68,26 @@ class SchemesController < ApplicationController
 
   def scheme_params
     @scheme_params ||= params.require(:scheme)
-                             .permit(:name, :channel)
+                             .permit(:name, channel_attributes: { name: [] })
+  end
+
+  def set_channel
+    from_channel, segment = from_channel?
+    unless from_channel
+      @channel = @scheme.latest_channel
+      return
+    end
+
+    previouse_channel = Channel.friendly.find(segment[:id])
+    @channel = @scheme.channels.find_by(device_type:  previouse_channel.device_type)
+  end
+
+  def from_channel?
+    return [false, nil] unless referer = request.referer
+    return [false, nil] unless segment = Rails.application.routes.recognize_path(referer)
+
+    from_channel = segment[:controller] == 'channels' && segment[:action] == 'show'
+
+    [from_channel, segment]
   end
 end
