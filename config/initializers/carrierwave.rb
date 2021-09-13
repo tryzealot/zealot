84aa218f42e3dcb2905d82015aff82c96aa52556
icyleaf <icyleaf.cn@gmail.com>
2021-09-13 13:22:07 +0800

    fix: 修复 ios 安装 plist 没有返回完整 icon url

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-CarrierWave.configure do |config|
-  url_options = Zealot.config.url_options
-  config.asset_host = "#{url_options[:protocol]}#{url_options[:host]}"
-  config.cache_dir = Rails.root.join('tmp', 'uploads', Rails.env)
+Rails.configuration.to_prepare do
+  CarrierWave.configure do |config|
+    url_options = Zealot.config.url_options
+    config.asset_host = "#{url_options[:protocol]}#{url_options[:host]}"
+    config.cache_dir = Rails.root.join('tmp', 'uploads', Rails.env)
+  end
 end
