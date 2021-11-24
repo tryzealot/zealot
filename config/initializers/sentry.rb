d868f197bf91e6ef30524d98ed142581759a2d6b
icyleaf <icyleaf.cn@gmail.com>
2021-11-24 11:01:00 +0800

    chore: 异常报错支持捕获 frame's local variable

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -6,8 +6,9 @@ if ENV['ZEALOT_SENTRY_DISABLE'].blank?
     Sentry.init do |config|
       config.dsn = ENV['ZEALOT_SENTRY_DNS'] || 'https://133aefa9f52448a1a7900ba9d02f93e1@sentry.io/1878137'
 
+      config.capture_exception_frame_locals = true
       config.rails.report_rescued_exceptions = true
-      config.breadcrumbs_logger = [:active_support_logger, :sentry_logger, :http_logger]
+      config.breadcrumbs_logger = %i[active_support_logger sentry_logger http_logger]
 
       config.send_default_pii = true
       config.environment = Rails.env
