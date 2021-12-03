3cdb59a822b2eea6ba213035ffce4fa077f741c1
icyleaf <icyleaf.cn@gmail.com>
2021-12-03 19:35:52 +0800

    feat: 版本 commit 会从 heroku 获取

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -185,7 +185,7 @@ class Setting < RailsSettings::Base
   # 版本信息（只读）
   scope :information do
     field :version, default: (ENV['ZEALOT_VERSION'] || 'development'), type: :string, readonly: true
-    field :vcs_ref, default: (ENV['ZEALOT_VCS_REF']), type: :string, readonly: true
+    field :vcs_ref, default: (ENV['ZEALOT_VCS_REF'] || ENV['HEROKU_SLUG_COMMIT']), type: :string, readonly: true
     field :build_date, default: ENV['ZEALOT_BUILD_DATE'], type: :string, readonly: true
   end
 
