4184525a7a69e8489bea936615e716f9ff8b85d7
icyleaf <icyleaf.cn@gmail.com>
2021-12-29 10:52:20 +0800

    refactor: 未登录模式允许在应用详情查看应用版本、分支、打包类型的筛选

--- app/policies/channel_policy.rb
+++ app/policies/channel_policy.rb
@@ -3,15 +3,15 @@
 class ChannelPolicy < ApplicationPolicy
 
   def versions?
-    guest_mode_or_signed_in?
+    true
   end
 
   def branches?
-    guest_mode_or_signed_in?
+    true
   end
 
   def release_types?
-    guest_mode_or_signed_in?
+    true
   end
 
   class Scope < Scope
