1226c167157702c642b75d47d72e67ab0d55956b
icyleaf <icyleaf.cn@gmail.com>
2021-12-29 10:51:20 +0800

    style: 调整屏幕宽度超过 1200px 双列的宽度

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def guest_mode_or_signed_in?
+    Setting.guest_mode? || user_signed_in?
+  end
+
   def roles_collection
     User.roles.to_a.collect { |c| [Setting.present_roles[c[0].to_sym], c[0]] }
   end
