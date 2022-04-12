20d001da5000defc49196ce19b801184af87c367
icyleaf <icyleaf.cn@gmail.com>
2022-04-12 12:07:19 +0800

    fix: rubocop

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 module DeviseHelper
-  def devise_provider_login_path(provider)
-    title = omniauth_display_name(provider)
-    key = title.downcase
+  def devise_provider_login_path(name)
+    fallback_title = omniauth_display_name(name)
+    key = fallback_title.downcase
 
-    url = [:user, provider.to_sym, :omniauth, :authorize]
+    provider = t("devise.shared.links.provider.#{key}", default: fallback_title)
+    title = t('devise.shared.links.sign_in_with_provider', provider: provider)
+    url = [:user, name.to_sym, :omniauth, :authorize]
     button_to(url, class: "btn btn-block btn-default text-left") do
-      raw(%Q(
-        #{t('devise.shared.links.sign_in_with_provider', provider: t("devise.shared.links.provider.#{key}", default: title))}
-        <i class="icon fab float-right fa-#{key}"></i>
-      ))
+      raw(%Q(#{title} <i class="icon fab float-right fa-#{key}"></i>))
     end
   end
 
