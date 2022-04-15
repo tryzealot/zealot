8e18306a4cc69549deaf3ed7d4ab115e2ec8e33b
icyleaf <icyleaf.cn@gmail.com>
2022-04-15 12:14:52 +0800

    refactor: 梳理 admin 页面的多语言结构

--- app/controllers/admin/web_hooks_controller.rb
+++ app/controllers/admin/web_hooks_controller.rb
@@ -4,18 +4,15 @@ class Admin::WebHooksController < ApplicationController
   before_action :set_web_hook, only: %i[show edit update destroy]
 
   def index
-    @title = t('web_hooks.title')
     @web_hooks = WebHook.all
     authorize @web_hooks
   end
 
   def show
     authorize @web_hook
-    @title = t('admin.web_hooks.show')
   end
 
   def new
-    @title = t('admin.web_hooks.new')
     @web_hook = WebHook.new
     authorize @web_hook
   end
@@ -25,12 +22,11 @@ class Admin::WebHooksController < ApplicationController
     authorize @web_hook
     return render :new unless @web_hook.save
 
-    redirect_to admin_users_url, notice: t('activerecord.success.create', key: t('web_hooks.title'))
+    redirect_to admin_users_url, notice: t('activerecord.success.create', key: t('admin.web_hooks.title'))
   end
 
   def edit
     authorize @web_hook
-    @title = t('admin.web_hooks.edit')
   end
 
   def update
@@ -38,13 +34,13 @@ class Admin::WebHooksController < ApplicationController
     channel_ids = web_hook_params.delete(:channel_ids)
     return render :edit unless @web_hook.update(web_hook_params)
 
-    redirect_to admin_web_hooks_url, notice: t('activerecord.success.update', key: t('web_hooks.title'))
+    redirect_to admin_web_hooks_url, notice: t('activerecord.success.update', key: t('admin.web_hooks.title'))
   end
 
   def destroy
     authorize @web_hook
     @web_hook.destroy
-    redirect_to admin_web_hooks_url, notice: t('activerecord.success.destroy', key: t('web_hooks.title'))
+    redirect_to admin_web_hooks_url, notice: t('activerecord.success.destroy', key: t('admin.web_hooks.title'))
   end
 
   private
