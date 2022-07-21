3f89dd87dc05c9841bb64687fb4f9d84898d726a
icyleaf <icyleaf.cn@gmail.com>
2022-07-21 16:47:04 +0800

    feat: 适配应用、首页等黑暗模式

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -9,8 +9,8 @@ Mime::Type.register 'text/plist', :plist
 Mime::Type.register 'application/vnd.iphone', :ipa
 Mime::Type.register 'application/vnd.android.package-archive', :apk
 
-ActionController::Renderers.add :qrcode do |string, options|
-  data = RQRCode::QRCode.new(string).as_png(options)
+ActionController::Renderers.add :qrcode do |content, options|
+  data = RQRCode::QRCode.new(content).as_png(options)
   self.content_type ||= Mime[:png]
   self.response_body = render_to_string(plain: data, template: nil)
 end
