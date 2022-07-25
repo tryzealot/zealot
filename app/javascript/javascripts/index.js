dc293d62926bb75b2012ff5edafe57b9b3975f54
icyleaf <icyleaf.cn@gmail.com>
2022-07-25 12:54:47 +0800

    fix: 暂时停用 ActionCable 来修复 Safari 无法正常使用 JS 的问题

---
+++ app/javascript/javascripts/index.js
@@ -0,0 +1,6 @@
+import "javascripts/base.js";
+import "javascripts/debug_files";
+import "javascripts/releases";
+import "javascripts/teardown/upload";
+import "javascripts/udid";
+import "javascripts/admin";
