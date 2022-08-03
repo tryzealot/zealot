06982277e7c7a24f1f3975daf7ce45df2302da86
icyleaf <icyleaf.cn@gmail.com>
2022-08-03 12:19:35 +0800

    feat: 实时刷新日志查看器

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,7 +3,6 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
-application.debug = false
 window.Stimulus   = application
 
 export { application }
