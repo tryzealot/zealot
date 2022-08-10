d9824e2252b686ed72f5a57148c6ac1deefc2dfb
icyleaf <icyleaf.cn@gmail.com>
2022-08-10 17:21:11 +0800

    feat: 根据设备展示获取 iOS 设备 udid 逻辑

--- app/javascript/controllers/udid_controller.js
+++ app/javascript/controllers/udid_controller.js
@@ -10,41 +10,47 @@ export default class extends Controller {
   }
 
   connect() {
-    if (!this.isPreviewFeature()) { return }
-
-    console.log(this.uaParser.getResult())
-    const iOS_1to12 = /iPad|iPhone|iPod/.test(navigator.platform)
-    const iOS13_iPad = (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
-    const iOS13_iPad2 = (navigator.userAgent.includes("Mac") && "ontouchend" in document)
-    const iOS1to12quirk = function() {
-      var audio = new Audio() // temporary Audio object
-      audio.volume = 0.5 // has no effect on iOS <= 12
-      return audio.volume === 1
-    }
-
-    let message = "usparse result: " + JSON.stringify(this.uaParser.getResult()) +
-      "\n iOS_1to12: " + iOS_1to12 +
-      "\n iOS13_iPad: " + iOS13_iPad +
-      "\n iOS13_iPad: " + iOS13_iPad +
-      "\n iOS13_iPad2: " + iOS13_iPad2 +
-      "\n iOS1to12quirk: " + iOS1to12quirk() +
-      "\n MSStream: " + !window.MSStream
-
-    this.debugTarget.value = message
-
     if (!this.isAppleOS()) {
       this.installTarget.classList.add("d-none")
       this.tipTarget.innerHTML = this.nonappleTipValue
-      return
+    } else if (this.isiOS()) {
+      this.qrcodeTarget.classList.add("d-none")
     }
 
+    if (this.isPreviewFeature()) {
+      console.log(this.uaParser.getResult())
+      const iOS_1to12 = /iPad|iPhone|iPod/.test(navigator.platform)
+      const iOS13_maxTouchPoints = (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+      const iOS13_ontouchend = (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+      const iOS1to12quirk = function() {
+        var audio = new Audio() // temporary Audio object
+        audio.volume = 0.5 // has no effect on iOS <= 12
+        return audio.volume === 1
+      }
 
+      let message = "usparse result: " + JSON.stringify(this.uaParser.getResult()) +
+        "\n iOS_1to12: " + iOS_1to12 +
+        "\n iOS13_maxTouchPoints: " + iOS13_maxTouchPoints +
+        "\n iOS13_ontouchend: " + iOS13_ontouchend +
+        "\n iOS1to12quirk: " + iOS1to12quirk() +
+        "\n MSStream: " + !window.MSStream
+
+      this.debugTarget.value = message
+    }
   }
 
   // Detect macOS/iOS/iPad OS
   isAppleOS() {
     let os = this.uaParser.getOS()
-    return os.name === 'Mac OS' || os.name === 'iOS'
+    return os.name === "Mac OS" || os.name === "iOS"
+  }
+
+  isiOS() {
+    let device = this.uaParser.getDevice()
+    let isiPhoneOriPod = device && (device.model === "iPhone" || device.model === "iPod")
+    let isiPad = device && (device.model === "iPad" || (navigator.userAgent.includes("Mac") && "ontouchend" in document))
+
+    return isiPhoneOriPod || isiPad
   }
 
   isPreviewFeature() {
