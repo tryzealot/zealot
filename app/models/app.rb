a8527fef2d9b5b6614a12fd686f6ed2541fdd6a6
icyleaf <icyleaf.cn@gmail.com>
2022-08-10 12:48:37 +0800

    feat: 补充重置演示数据

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class App < ApplicationRecord
-  scope :avaiable_debug_files, -> { joins(:debug_files).distinct }
-
   has_and_belongs_to_many :users
   has_many :schemes, dependent: :destroy
   has_many :debug_files, dependent: :destroy
 
   accepts_nested_attributes_for :schemes
+
+  scope :avaiable_debug_files, -> { joins(:debug_files).distinct }
 
   validates :name, presence: true
 
