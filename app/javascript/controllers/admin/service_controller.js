2f1d52398acc4ac990a838edf41d05a79dd4528c
icyleaf <icyleaf.cn@gmail.com>
2022-08-11 15:18:34 +0800

    fix: 删除需要重启的系统参数也需要提示

--- app/javascript/controllers/admin/service_controller.js
+++ app/javascript/controllers/admin/service_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import { Zealot } from "../zealot"
+import { poll } from "../utils"
 import jquery from "jquery"
 
 const RESTART_URI = "admin/service/restart"
@@ -15,58 +16,45 @@ export default class extends Controller {
   async restart() {
     const buttonHTML = this.buttonTarget.innerHTML
 
+    // Update status of button
     this.clearNotifcation()
     this.updateRestaringState()
 
-    var serverRestarting = true;
-    const restartSuccess = await this.serviceRestart()
+    // Do restart action
+    await this.serviceRestart()
     await this.sleep(2000)
 
-    if (!restartSuccess) {
-      this.buttonTarget.innerHTML = buttonHTML
-      return false
+    // Run loop to check service is back online
+    const healthCheck = async () => {
+      const response = await fetch(Zealot.rootUrl + HEALTH_CHECK_URI)
+      return response && response.status === 200
     }
 
-    do {
-      var online = await this.serviceisOnline()
-      if (online) {
-        serverRestarting = false
-      } else {
-        await this.sleep(1000)
-      }
-    } while (serverRestarting)
+    const result = await poll({
+      fn: healthCheck,
+      validate: (online) => !!online,
+      interval: 2000
+    })
 
-    this.updateRestartedState()
-    await this.sleep(2000)
-    window.location.reload()
+    if (result) {
+      this.updateRestartedState()
+      await this.sleep(2000)
+      window.location.reload()
+    }
   }
 
   serviceRestart() {
     fetch(Zealot.rootUrl + RESTART_URI, {
         method: "POST"
       })
-      .then(response => response.json())
-      .then(() => {
-        return true
+      .then((response) => response.status === 200)
+      .then((result) => {
+        return result
       })
       .catch((error) => {
         console.debug("service restart failed", error)
         return false
     })
-  }
-
-  serviceisOnline() {
-    fetch(Zealot.rootUrl + HEALTH_CHECK_URI, {
-        method: "GET"
-      })
-      .then(response => response.json())
-      .then(() => {
-        return true
-      })
-      .catch((error) => {
-        console.debug("service is down, stilling wait", error)
-        return false
-      })
   }
 
   clearNotifcation() {
