83cb9fbe9decf8086c9d05c397a246fa5a1ac870
icyleaf <icyleaf.cn@gmail.com>
2022-10-31 19:42:29 +0800

    style: 获取 iOS 设备 UDID 二维码支持黑暗模式

--- app/controllers/releases/qrcode_controller.rb
+++ app/controllers/releases/qrcode_controller.rb
@@ -1,55 +1,18 @@
 # frozen_string_literal: true
 
 class Releases::QrcodeController < ApplicationController
-  before_action :set_release
+  include Qrcode
 
-  THEMES = {
-    light: {
-      fill: '#FFFFFF',
-      color: '#465960'
-    },
-    dark: {
-      fill: '#343a40',
-      color: '#F0F4Fb'
-    },
-  }
+  before_action :set_release
 
   ##
   # 显示应用的二维码
   # GET /apps/:slug/(:version)/qrcode
   def show
-    render qrcode: friendly_channel_release_url(@release.channel, @release), **options
+    render qrcode: friendly_channel_release_url(@release.channel, @release), **qrcode_options
   end
 
   private
-
-  def options
-    {
-      module_px_size: px_size,
-      fill: theme[:fill],
-      color: theme[:color]
-    }
-  end
-
-  def theme
-    @theme ||= -> do
-      name = params.fetch(:theme, 'light') == 'light' ? :light : :dark
-      THEMES[name]
-    end.call
-  end
-
-  def px_size
-    case params[:size]
-    when 'thumb'
-      3
-    when 'medium'
-      5
-    when 'large'
-      6
-    else
-      2
-    end
-  end
 
   def set_release
     @release = Release.find params[:release_id]
