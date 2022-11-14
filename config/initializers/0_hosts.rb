57133334bd623cc0916d55e6236fe12625fc7551
icyleaf <icyleaf.cn@gmail.com>
2022-11-14 15:41:14 +0800

    feat: 开放 CORS

--- config/initializers/0_hosts.rb
+++ config/initializers/0_hosts.rb
@@ -6,11 +6,11 @@ Rails.configuration.to_prepare do
 
   # Configure ActionCable request protection
   Rails.application.configure do
+    config.action_mailer.default_url_options = url_options
+
     if ENV.fetch('ZEALOT_DISABLE_CABLE_REQUEST_PROTECTION') { 'false' } == 'true'
       config.action_cable.disable_request_forgery_protection = true
     else
-      config.action_mailer.default_url_options = url_options
-
       config.action_cable.allowed_request_origins = [
         /http(s)?:\/\/#{Setting.site_domain}/
       ]
