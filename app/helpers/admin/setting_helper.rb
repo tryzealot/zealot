475e3815190680fd9b6a2d82b6db6afeb3ddf2d1
icyleaf <icyleaf.cn@gmail.com>
2023-02-28 11:30:45 +0800

    feat: 修改版本号规范和 Git 哈希值链接规则

--- app/helpers/admin/setting_helper.rb
+++ app/helpers/admin/setting_helper.rb
@@ -3,12 +3,18 @@
 module Admin
   module SettingHelper
     def vcs_ref_link(ref)
-      base = Rails.env.development? ? 'main' : Setting.version
-      link_to ref, github_repo_compare_commit(ref, base), target: :blank
+      link = if Rails.env.development?
+          github_repo_compare_commit(ref, 'main')
+        else
+          docker_tag? ? github_repo_compare_commit(ref, Setting.version) : github_version_link
+        end
+
+      link_to ref, link, target: :blank
     end
 
     def zealot_version(suffix: false)
       version = Setting.version
+      return "#{version}-dev" if Rails.env.development?
       return version if !docker_tag? || !suffix
 
       "#{version}-#{ENV['DOCKER_TAG']}"
@@ -18,6 +24,10 @@ module Admin
 
     def github_repo_compare_commit(target, base)
       "#{Setting.repo_url}/compare/#{base}...#{target}"
+    end
+
+    def github_version_link
+      "#{Setting.repo_url}/releases/tag/#{Setting.version}"
     end
 
     def docker_tag?
