1a65135015fd9c041575c29d741b434bb38bf264
icyleaf <icyleaf.cn@gmail.com>
2023-04-12 11:32:26 +0800

    feat: app 获取最新上传版本增加缓存

--- app/models/app.rb
+++ app/models/app.rb
@@ -13,11 +13,13 @@ class App < ApplicationRecord
   validates :name, presence: true
 
   def recently_release
-    return unless scheme = schemes.take
-    return unless channel = scheme.channels.take
-    return unless release = channel.releases.last
+    Rails.cache.fetch("app_#{id}_recently_release") do
+      return unless schcmes_ids = schemes.select(:id).map(&:id)
+      return unless channel_ids = Channel.select(:id).where(schemes: schcmes_ids).map(&:id)
+      return unless release = Release.where(channels: channel_ids).last
 
-    release
+      release
+    end
   end
 
   def total_schemes
