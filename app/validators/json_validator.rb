957b3bf75f08be4c8d3f947d3b6b92e9302f2046
icyleaf <icyleaf.cn@gmail.com>
2023-04-23 18:11:54 +0800

    fix: 无法保存 json 格式

--- app/validators/json_validator.rb
+++ app/validators/json_validator.rb
@@ -8,6 +8,7 @@ class JsonValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     return if value.blank? && @allow_empty
+    return unless value.is_a?(String)
 
     JSON.parse(value)
   rescue JSON::ParserError
