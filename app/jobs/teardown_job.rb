3c39cf8a4c4e07ecbf8c41abfc7f3a314cbcf2cc
icyleaf <icyleaf.cn@gmail.com>
2023-04-27 10:27:05 +0800

    fix: 忽略无法解析的后台任务

--- app/jobs/teardown_job.rb
+++ app/jobs/teardown_job.rb
@@ -7,6 +7,11 @@ class TeardownJob < ApplicationJob
     return unless file = determine_file!(release_id)
 
     metadata = TeardownService.new(file.path).call
+    unless metadata
+      logger.error "Unable to parse metadata with release: #{release_id}"
+      return
+    end
+
     metadata.update_attribute(:user_id, user_id) if user_id.present?
     update_release_resouces(release_id, metadata)
   rescue AppInfo::UnknownFormatError
