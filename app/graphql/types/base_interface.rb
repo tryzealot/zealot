6f1e3db4f8c8dbac8ef43c0ea587945a7a44e040
icyleaf <icyleaf.cn@gmail.com>
2023-08-01 16:27:34 +0800

    fix: graphql auth user (pass by bearer token)

--- app/graphql/types/base_interface.rb
+++ app/graphql/types/base_interface.rb
@@ -3,8 +3,8 @@
 module Types
   module BaseInterface
     include GraphQL::Schema::Interface
-    edge_type_class(Types::BaseEdge)
-    connection_type_class(Types::BaseConnection)
+    # edge_type_class(Types::BaseEdge)
+    # connection_type_class(Types::BaseConnection)
 
     field_class Types::BaseField
   end
