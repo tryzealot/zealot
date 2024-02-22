7a8fac64d2049e8abb80eb7909ba3cb26a0330bc
icyleaf <icyleaf.cn@gmail.com>
2024-02-22 14:03:08 +0000

    fix: 应用版本详情匹配调试文件异常
    relates #1406

--- app/models/release.rb
+++ app/models/release.rb
@@ -174,7 +174,7 @@ class Release < ApplicationRecord
 
     debug_files.select do |debug_file|
       if ios?
-        debug_file.metadata.where('data -> identifier = ?', bundle_id).count > 0
+        debug_file.metadata.where("data->>'identifier' = ?", bundle_id).count > 0
       elsif android?
         debug_file.metadata.where(object: bundle_id).count > 0
       end
