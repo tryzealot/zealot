f42983cb11ee36f799ce311e6e3f65a2937a7449
icyleaf <icyleaf.cn@gmail.com>
2024-05-08 09:32:33 +0000

    api: add create, update and destroy app

--- app/controllers/api/apps_controller.rb
+++ app/controllers/api/apps_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::AppsController < Api::BaseController
   before_action :validate_user_token
-  before_action :set_app, only: %i[show]
+  before_action :set_app, only: %i[show update destroy]
 
   # GET /api/apps
   def index
@@ -15,9 +15,31 @@ class Api::AppsController < Api::BaseController
     render json: @app, serializer: Api::AppSerializer, include: 'schemes.channels'
   end
 
+  # POST /api/apps
+  def create
+    @app = App.create!(app_params)
+    render json: @app, serializer: Api::AppSerializer, include: 'schemes.channels', status: :created
+  end
+
+  # PUT /api/apps/:id
+  def update
+    @app.update!(app_params)
+    render json: @app, serializer: Api::AppSerializer, include: 'schemes.channels'
+  end
+
+  # DELETE /api/apps/:id
+  def destroy
+    @app.destroy!
+    render json: {}
+  end
+
   protected
 
   def set_app
     @app = App.find(params[:id])
+  end
+
+  def app_params
+    @app_params ||= params.permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -163,18 +163,15 @@ Rails.application.routes.draw do
   # API v1
   #############################################
   namespace :api do
-    namespace :apps do
-      post 'upload', to: 'upload#create'
+    resources :apps, except: %i[new edit] do
+      collection do
+        post :upload, to: 'apps/upload#create'
 
-      get 'latest', to: 'latest#show'
-      get 'version_exist', to: 'version_exist#show'
-      get 'versions', to: 'versions#index'
-      get 'versions/(:release_version)', to: 'versions#show'
-
-      get ':id', action: :show
-      patch ':id', action: :update
-      delete ':id', action: :destroy
-      get '', action: :index
+        get :latest, to: 'apps/latest#show'
+        get :version_exist, to: 'apps/version_exist#show'
+        get :versions, to: 'apps/versions#index'
+        get 'versions/(:release_version)', to: 'apps/versions#show'
+      end
     end
 
     resources :debug_files, except: %i[new edit create] do
