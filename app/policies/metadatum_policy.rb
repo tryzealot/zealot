241794fbdda66d402fd43bf32054a4b5ee496c3a
icyleaf <icyleaf.cn@gmail.com>
2024-05-17 15:16:32 +0800

    fix: teardown in demo mode

--- app/policies/metadatum_policy.rb
+++ app/policies/metadatum_policy.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 class MetadatumPolicy < ApplicationPolicy
-
-  def show?
-    guest_mode_or_signed_in?
-  end
-
   class Scope < Scope
     def resolve
       scope.all
