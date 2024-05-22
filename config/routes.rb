d85d525bd4be817f6dfec31253c30c969677dff1
icyleaf <icyleaf.cn@gmail.com>
2024-05-22 11:48:46 +0000

    fix: confirmation email handle error

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   #############################################
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
   }
 
   #############################################
