fec74f119f10d203fefafdeeaeef8c908f59bf69
icyleaf <icyleaf.cn@gmail.com>
2024-05-22 13:19:05 +0000

    feat(web): api users can update customize settings

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class UserSerializer < ApplicationSerializer
-  attributes :id, :username, :email
+  attributes :id, :username, :email, :locale, :appearance, :timezone
   attribute :role, if: -> { scope.admin? }
 end
