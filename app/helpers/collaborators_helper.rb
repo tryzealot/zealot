104183193ba4979c69ce43b3d5bd759ff4ecc25e
icyleaf <icyleaf.cn@gmail.com>
2024-05-22 08:07:31 +0000

    feat(web): add collaborator owner who creates the App

--- app/helpers/collaborators_helper.rb
+++ app/helpers/collaborators_helper.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 module CollaboratorsHelper
-  def not_same_user?(collaborator)
-    collaborator.user != current_user
+  def same_user?(collaborator)
+    collaborator.user == current_user
   end
 
   def edit_role_user?(collaborator)
     target_role = collaborator.role
-    if current_user.manage?
-      return User.roles[target_role] < Collaborator.roles[current_user.role]
-    end
+    return true if current_user.manage?
+    return true if current_user.app_roles?(collaborator.app, :admin)
 
-    source_role = Collaborator.find_by(user: current_user, app: collaborator.app).role
-    Collaborator.roles[target_role] < Collaborator.roles[source_role]
+    false
   end
 end
