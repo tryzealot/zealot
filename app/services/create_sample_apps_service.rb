c8a8a9d1998c0dd9d12d93c925c0ca19ea0e3904
icyleaf <icyleaf.cn@gmail.com>
2024-05-23 02:05:11 +0000

    fix(demo): rename android channels name

--- app/services/create_sample_apps_service.rb
+++ app/services/create_sample_apps_service.rb
@@ -3,8 +3,6 @@
 require 'securerandom'
 
 class CreateSampleAppsService
-  include ActionView::Helpers::TranslationHelper
-
   RELEASE_COUNT = 3
 
   def call(user)
@@ -15,29 +13,29 @@ class CreateSampleAppsService
   private
 
   def stardford_app(user)
-    app_name = t('demo.app_name1')
+    app_name = I18n.t('demo.app_name1')
     app_bundle_id = 'com.zealot.app-demo'
     channels = %i[Android iOS]
     schemes = [
-      t('settings.preset_schemes.beta'),
-      t('settings.preset_schemes.adhoc'),
-      t('settings.preset_schemes.production'),
+      I18n.t('settings.preset_schemes.beta'),
+      I18n.t('settings.preset_schemes.adhoc'),
+      I18n.t('settings.preset_schemes.production'),
     ]
     changelog = [
       {
-        author: t('settings.preset_role.developer'),
+        author: I18n.t('settings.preset_role.developer'),
         date: '2019-10-24 23:0:24 +0800',
         message: 'bump 0.1.0',
         email: 'admin@zealt.com'
       },
       {
-        author: t('settings.preset_role.developer'),
+        author: I18n.t('settings.preset_role.developer'),
         date: '2019-10-23 17:41:41 +0800',
         message: 'fix: xxx',
         email: 'admin@zealt.com'
       },
       {
-        author: t('settings.preset_role.developer'),
+        author: I18n.t('settings.preset_role.developer'),
         date: '2019-10-22 11:11:11 +0800',
         message: 'feat: xxx done',
         email: 'admin@zealt.com'
@@ -53,11 +51,11 @@ class CreateSampleAppsService
                                                     device_type: channel_name.downcase.to_sym
         bundle_id = generate_bundle_id app_bundle_id, channel
         release_type = case scheme.name
-                       when t('settings.preset_schemes.beta')
+                       when I18n.t('settings.preset_schemes.beta')
                          'beta'
-                       when t('settings.preset_schemes.adhoc')
+                       when I18n.t('settings.preset_schemes.adhoc')
                          'adhoc'
-                       when t('settings.preset_schemes.production')
+                       when I18n.t('settings.preset_schemes.production')
                          'release'
                        end
 
@@ -76,10 +74,10 @@ class CreateSampleAppsService
   end
 
   def android_channels_app(user)
-    app_name = t('demo.app_name2')
+    app_name = I18n.t('demo.app_name2')
     app_bundle_id = 'com.zealot.android.app-demo'
-    schemes = [ t('settings.preset_schemes.production') ]
-    channels = t('demo.android_channels').values
+    schemes = [ I18n.t('settings.preset_schemes.production') ]
+    channels = I18n.t('demo.android_channels').values
     changelog = "bump 0.1.0\nfix: xxx\nfeat: xxx done"
 
     app = create_app(app_name, user)
@@ -157,7 +155,7 @@ class CreateSampleAppsService
 
   def create_app(name, user)
     owner = user.is_a?(Array) ? user.first : user
-    app = App.first_or_initialize name: name
+    app = App.find_or_initialize_by(name:)
     return app unless app.new_record?
 
     app.save!
