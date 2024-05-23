e858813139f43ccbeda945a6ccc9cccdb345b0b6
icyleaf <icyleaf.cn@gmail.com>
2024-05-23 02:47:23 +0000

    fix(demo): can not edit user profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  def update
-    if helpers.default_admin_in_demo_mode?(resource)
-      return redirect_to edit_user_registration_url, alert: t('errors.messages.invaild_in_demo_mode')
-    end
-
-    super
-  end
 
   protected
 
