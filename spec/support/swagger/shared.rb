f880cfca77f48fd37bba91a2c8de8e8b0e803c6e
icyleaf <icyleaf.cn@gmail.com>
2024-05-24 08:52:08 +0000

    refactor(api): change debug_file response data struct by category

---
+++ spec/support/swagger/shared.rb
@@ -0,0 +1,30 @@
+
+# Paramters
+shared_examples :pagiation_parameters do
+  parameter '$ref': '#/components/parameters/pageParam'
+  parameter '$ref': '#/components/parameters/perPageParam'
+end
+
+shared_examples :channel_key_parameter do
+  parameter '$ref': '#/components/parameters/channelKeyParam'
+end
+
+shared_examples :version_parameters do
+  parameter '$ref': '#/components/parameters/releaseVersionParam'
+  parameter '$ref': '#/components/parameters/buildVersionParam'
+end
+
+# Responses
+shared_examples :unauthorized_response do
+  response 401, I18n.t('api.responses.unauthorized.description') do
+    schema '$ref' => '#/components/responses/Unauthorized'
+    run_test!
+  end
+end
+
+shared_examples :not_found_response do |resource_name, **options|
+  response 404, I18n.t(:description, scope: [:api, :responses, :not_found], default: :default, model: resource_name) do #'api.responses.not_found.description') do
+    schema '$ref' => '#/components/responses/NotFound'
+    run_test!
+  end
+end
