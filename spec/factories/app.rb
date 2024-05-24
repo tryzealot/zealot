1bb79a78c55a59d3f3ebe9f97083a819ae630bd0
icyleaf <icyleaf.cn@gmail.com>
2024-05-24 08:52:08 +0000

    refactor(api): change debug_file response data struct by category

---
+++ spec/factories/app.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  sequence(:name) { |n| "App #{n}" }
+
+  factory :app do
+    name { generate(:name) }
+  end
+
+  factory :all_apps, class: "App" do
+    title { generate(:name) }
+  end
+end
