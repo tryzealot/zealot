f504c28d703952363dd04a0907e3161060864920
icyleaf <icyleaf.cn@gmail.com>
2024-05-24 08:52:08 +0000

    refactor(api): change debug_file response data struct by category

--- app/serializers/api/debug_file_serializer.rb
+++ app/serializers/api/debug_file_serializer.rb
@@ -3,6 +3,7 @@
 class Api::DebugFileSerializer < ApplicationSerializer
   # DebugFIle model based
   attributes :id, :app_name, :device_type, :release_version, :build_version, :file_url
+  attribute :filesize, key: :file_size
   has_many :metadata
 
   def app_name
