6a3fcbb026864dc12bec952a36a11f735187ec6d
icyleaf <icyleaf.cn@gmail.com>
2024-05-25 16:07:38 +0000

    feat(web): bulk operation to destroy mutiple builds

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,6 +1,8 @@
 import { Application } from "@hotwired/stimulus"
+import CheckboxSelectAll from "@stimulus-components/checkbox-select-all"
 
 const application = Application.start()
+application.register("checkbox-select-all", CheckboxSelectAll)
 
 // Configure Stimulus development experience
 window.Stimulus   = application
