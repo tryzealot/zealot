dc17f7815df122c52eaddf73b98a2861d8ad4fca
icyleaf <icyleaf.cn@gmail.com>
2024-06-19 02:45:12 +0000

    fix(web): unknown error with bootstrap and popver

--- app/javascript/controllers/global_controller.js
+++ app/javascript/controllers/global_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 import * as ActionCable from "@rails/actioncable"
-import * as bootstrap from "bootstrap"
 import { Zealot } from "./zealot"
 import { application } from "./application"
-import { PushMenu, Defaults } from "admin-lte"
 
 export default class extends Controller {
   static values = {
@@ -15,7 +13,6 @@ export default class extends Controller {
   connect() {
     this.initZealot()
     this.setupRailsDebugMode()
-    this.fixAdminlteWithTubros()
     this.switchDarkMode()
   }
 
@@ -29,43 +26,10 @@ export default class extends Controller {
     const apperance = Zealot.siteApperance
     if (apperance === "dark" || (apperance === "auto" && Zealot.isDarkMode())) {
       document.documentElement.setAttribute("data-bs-theme", "dark");
-
-      // var mainHeader = document.getElementsByClassName("app-header")
-      // Array.prototype.forEach.call(mainHeader, function(element) {
-      //   element.classList.replace("navbar-white", "navbar-dark")
-      // })
-
-      // var mainSidebar = document.getElementsByClassName("app-sidebar")
-      // Array.prototype.forEach.call(mainSidebar, function(element) {
-      //   element.classList.replace("sidebar-light-primary", "sidebar-dark-primary")
-      // });
     } else if (apperance === "light" && Zealot.isDarkMode()) {
-
       var darkBrandImage = document.getElementsByClassName("dark-brand-image")
-      Array.prototype.forEach.call(darkBrandImage, function(element) {
+      darkBrandImage.forEach(element => {
         element.remove()
-      });
-    }
-  }
-
-  fixAdminlteWithTubros() {
-    this.enableTooltips()
-    this.fixSidebarResize()
-  }
-
-  enableTooltips() {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl, {
-      placement: "top"
-    }))
-  }
-
-  fixSidebarResize() {
-    const sidebar = document.querySelector(".app-sidebar")
-    if (sidebar) {
-      window.addEventListener("turbo:load", () => {
-        const data = new PushMenu(sidebar, Defaults)
-        data.init()
       })
     }
   }
--- app/javascript/frameworks.js
+++ app/javascript/frameworks.js
@@ -1,3 +1,46 @@
-import "bootstrap"
-import "admin-lte"
 import "@hotwired/turbo-rails"
+import * as bootstrap from "bootstrap"
+import { PushMenu } from "admin-lte"
+
+window.addEventListener("turbo:load", () => {
+  // Trigger bootstrap tooltip
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  Array.prototype.forEach.call(tooltipTriggerList, function(element) {
+    new bootstrap.Tooltip(element)
+  })
+
+  // // Trigger adminlte sidebar toggle
+  // const sidebar_toggle = '[data-lte-toggle="sidebar"]'
+  // const sidebarTriggerList = document.querySelectorAll(sidebar_toggle)
+  // sidebarTriggerList.forEach(btn => {
+  //   btn.addEventListener('click', event => {
+  //     event.preventDefault()
+  //     let button = event.currentTarget
+
+  //     if (button?.dataset.lteToggle !== 'sidebar') {
+  //       button = button?.closest(sidebar_toggle)
+  //     }
+
+  //     if (button) {
+  //       event?.preventDefault()
+  //       const data = new PushMenu(button, {
+  //         sidebarBreakpoint: 992
+  //       })
+  //       data.toggle()
+  //     }
+  //   })
+  // })
+
+  // const sidebar = document.querySelector(".app-sidebar")
+  // if (sidebar) {
+  //   const data = new PushMenu(sidebar, {
+  //     sidebarBreakpoint: 992
+  //   })
+  //   data.init()
+
+  //   window.addEventListener('resize', () => {
+  //     data.init()
+  //   })
+  // }
+})
+
