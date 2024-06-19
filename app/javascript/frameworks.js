be32d9de53700658dd8f7eb168b021b96aff9297
icyleaf <icyleaf.cn@gmail.com>
2024-06-19 07:37:43 +0000

    refactor(style): migrate card collapse and sidebar toggle

--- app/javascript/frameworks.js
+++ app/javascript/frameworks.js
@@ -1,6 +1,6 @@
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap"
-import { PushMenu } from "admin-lte"
+import { PushMenu, CardWidget } from "admin-lte"
 
 window.addEventListener("turbo:load", () => {
   // Trigger bootstrap tooltip
@@ -9,27 +9,38 @@ window.addEventListener("turbo:load", () => {
     new bootstrap.Tooltip(element)
   })
 
-  // // Trigger adminlte sidebar toggle
-  // const sidebar_toggle = '[data-lte-toggle="sidebar"]'
-  // const sidebarTriggerList = document.querySelectorAll(sidebar_toggle)
-  // sidebarTriggerList.forEach(btn => {
-  //   btn.addEventListener('click', event => {
-  //     event.preventDefault()
-  //     let button = event.currentTarget
+  const cardCollapseButtonsList = document.querySelectorAll("[data-lte-toggle=\"card-collapse\"]")
+  cardCollapseButtonsList.forEach((btn) => {
+    btn.addEventListener('click', event => {
+      event.preventDefault()
 
-  //     if (button?.dataset.lteToggle !== 'sidebar') {
-  //       button = button?.closest(sidebar_toggle)
-  //     }
+      const target = event.target
+      const data = new CardWidget(target)
+      data.toggle()
+    })
+  })
 
-  //     if (button) {
-  //       event?.preventDefault()
-  //       const data = new PushMenu(button, {
-  //         sidebarBreakpoint: 992
-  //       })
-  //       data.toggle()
-  //     }
-  //   })
-  // })
+  // Trigger adminlte sidebar toggle
+  const sidebar_toggle = '[data-lte-toggle="sidebar"]'
+  const sidebarTriggerList = document.querySelectorAll(sidebar_toggle)
+  sidebarTriggerList.forEach(btn => {
+    btn.addEventListener('click', event => {
+      event.preventDefault()
+      let button = event.currentTarget
+
+      if (button?.dataset.lteToggle !== 'sidebar') {
+        button = button?.closest(sidebar_toggle)
+      }
+
+      if (button) {
+        event?.preventDefault()
+        const data = new PushMenu(button, {
+          sidebarBreakpoint: 992
+        })
+        data.toggle()
+      }
+    })
+  })
 
   // const sidebar = document.querySelector(".app-sidebar")
   // if (sidebar) {
