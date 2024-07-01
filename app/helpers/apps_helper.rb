1a874ee932c2bdac9086ff02482aaded29942538
icyleaf <icyleaf.cn@gmail.com>
2024-07-01 05:06:59 +0000

    refactor(web): enable part of turbo in release page

--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -72,7 +72,7 @@ module AppsHelper
 
     title = release_type_name(release_type)
     if params[:name] != release_type && user_signed_in_or_guest_mode?
-      link_to(title, friendly_channel_release_types_path(release.channel, name: release_type))
+      link_to(title, friendly_channel_release_types_path(release.channel, name: release_type), data: { turbo: false })
     else
       title
     end
