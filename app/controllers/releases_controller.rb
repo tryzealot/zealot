90984a187a5cf3d2dfed46e6285486fd25bd3d42
Rakuyo <rakuyo.mo@gmail.com>
2024-11-27 10:24:32 +0800

    style: Add line breaks

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -91,7 +91,8 @@ class ReleasesController < ApplicationController
 
   def release_params
     params.require(:release).permit(
-      :file, :changelog, :release_version, :build_version, :release_type, :branch, :git_commit, :ci_url, :use_original_filename
+      :file, :changelog, :release_version, :build_version, :release_type, :branch, :git_commit, 
+      :ci_url, :use_original_filename
     )
   end
 
