dd3058289ee97f8a151615c59a41e176f6cc63c8
icyleaf <icyleaf.cn@gmail.com>
2025-05-19 02:56:04 +0000

    feat(api): add health api

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -9,4 +9,14 @@ HealthCheck.setup do |config|
   ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST']
   ip_whitelist = ip_whitelist.split(',').select(&:present?).map(&:strip) if ip_whitelist.present?
   config.origin_ip_whitelist = ip_whitelist if ip_whitelist.present?
+
+  # Text output upon success
+  config.success = 'healthy'
+
+  # Text output upon failure
+  config.failure = 'unhealthy'
+
+  # text and object(json/xml) failure codes are set separately
+  config.http_status_for_error_text = 500
+  config.http_status_for_error_object = 500
 end
