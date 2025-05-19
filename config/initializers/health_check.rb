d443b3600654110fd2cf673898802becc9464bc9
icyleaf <icyleaf.cn@gmail.com>
2025-05-19 03:26:34 +0000

    feat: add background job check in full health check (not enabled)

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
+# require_relative '../../lib/zealot/goodjob_health_check'
+
 HealthCheck.setup do |config|
   config.uri = 'health'
 
   config.standard_checks = %w[database cache]
   config.full_checks = %w[database migrations cache]
+
+  # config.add_custom_check('background_job') do
+  #   Zealot::GoodjobHealthCheck.check
+  # end
 
   ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST']
   ip_whitelist = ip_whitelist.split(',').select(&:present?).map(&:strip) if ip_whitelist.present?
