b7d2e9266b3e8dd9e1fad2e987e7d0608f77a821
icyleaf <icyleaf.cn@gmail.com>
2025-05-19 01:28:48 +0000

    feat(api): allow /heahlth api public

--- config/initializers/health_check.rb
+++ config/initializers/health_check.rb
@@ -6,7 +6,7 @@ HealthCheck.setup do |config|
   config.standard_checks = %w[database cache]
   config.full_checks = %w[database migrations cache]
 
-  ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST'] || '127.0.0.1'
+  ip_whitelist = ENV['ZEALOT_HEALTH_CHECK_IP_WHITELIST']
   ip_whitelist = ip_whitelist.split(',').select(&:present?).map(&:strip) if ip_whitelist.present?
   config.origin_ip_whitelist = ip_whitelist if ip_whitelist.present?
 end
