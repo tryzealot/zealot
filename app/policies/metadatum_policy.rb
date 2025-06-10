7e9c13491b6be838a2df6eac5f32a6c64b1e4919
icyleaf <icyleaf.cn@gmail.com>
2025-06-10 03:14:26 +0000

    fix(web): security issue tear down id scope

--- app/policies/metadatum_policy.rb
+++ app/policies/metadatum_policy.rb
@@ -2,17 +2,40 @@
 
 class MetadatumPolicy < ApplicationPolicy
 
+  def show?
+    user_signed_in? && (owner? || any_manage? || app_member?)
+  end
+
   def new?
     user_signed_in?
   end
 
   def destroy?
-    user_signed_in?
+    user_signed_in? && (owner? || any_manage? || app_member?)
   end
 
   class Scope < Scope
     def resolve
       scope.all
     end
+  end
+
+  private
+
+  def any_manage?
+    return true if manage?
+    return false unless app = record.app
+
+    manage?(app: app)
+  end
+
+  def app_member?
+    return false unless app = record.app
+
+    app_collaborator?(user, app)
+  end
+
+  def owner?
+    record.user == user
   end
 end
