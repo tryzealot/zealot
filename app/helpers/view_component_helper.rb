2048222717246a6682335e503cd84f98bf69f882
icyleaf <icyleaf.cn@gmail.com>
2025-11-27 08:54:48 +0000

    refactor(web): remove confirm modal view component

--- app/helpers/view_component_helper.rb
+++ app/helpers/view_component_helper.rb
@@ -4,13 +4,4 @@ module ViewComponentHelper
   def render_modal(**options, &)
     render ModalComponent.new(**options), &
   end
-
-  def render_confirm_modal(**options, &)
-    raise ArgumentError, 'confirm_link is required' unless options.key?(:confirm_link)
-
-    options[:confirm_value] ||= t('modals.buttons.confirm')
-    options[:cancel_value] ||= t('modals.buttons.cancel')
-    options[:tooltip_value] ||= t('modals.tooltip.delete')
-    render(ConfirmModalComponent.new(body, confirm_link: confirm_link, title: title, **options), &)
-  end
 end
