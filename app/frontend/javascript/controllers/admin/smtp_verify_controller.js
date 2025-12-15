d519ae640ef678a7322895909e72e8d6564fa03b
icyleaf <icyleaf.cn@gmail.com>
2025-12-15 15:29:03 +0000

    refactor(web): send notification after test smtp server

--- app/frontend/javascript/controllers/admin/smtp_verify_controller.js
+++ app/frontend/javascript/controllers/admin/smtp_verify_controller.js
@@ -1,34 +1,27 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
+import { turboStream } from "../../utils/helpers";
 
 const VERIFY_URI = "/admin/services/smtp_verify"
 
 export default class extends Controller {
   static values = {
-    uri: String,
-    inprocess: String,
-    success: String,
-    failed: String
+    testing: String,
   }
 
-  run(event) {
+  async verify(event) {
     const target = event.target
-    target.innerHTML = this.inprocessValue
+    const restoreValue = target.innerText
 
-    fetch(VERIFY_URI, {
-      method: "POST"
-    })
-    .then((response) => {
-      if (response.status === 200) {
-        console.info("smtp verify success")
-        target.innerHTML = this.successValue
-      } else {
-        response.json().then((body) => {
-          console.error(`smtp verify failed: ${body.message}`)
-          target.innerHTML = `${this.failedValue}: ${body.message}`
-          target.classList.remove("bg-primary")
-          target.classList.add("bg-danger")
-        })
-      }
-    })
+    target.innerHTML = ""
+    target.appendChild(this.loadingNode())
+    target.appendChild(document.createTextNode(this.inprocessValue))
+    await turboStream(VERIFY_URI, { method: "POST" })
+    target.innerText = restoreValue
+  }
+
+  loadingNode() {
+    const span = document.createElement("span")
+    span.classList.add("d-loading", "d-loading-spinner")
+    return span
   }
 }
