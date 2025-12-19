c77e4bd0130a869da7986f3dcd79f7018d2f7fcd
icyleaf <icyleaf.cn@gmail.com>
2025-12-19 06:00:21 +0000

    refactor(job): turbo stream broadcast accept user is blank

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -30,13 +30,18 @@ class ApplicationJob < ActiveJob::Base
     turbo_stream(method: :broadcast_append_to, user_id: user_id, target: target, html: html)
   end
 
-  def turbo_stream(method:, user_id:, target:, **options)
+  def turbo_stream(method:, user_id: nil, target:, **options)
     return if user_id.blank?
 
-    user = User.find(user_id)
-    streamables = options.delete(:streamables) || nil
-
+    user = user_id ? User.find(user_id) : nil
+    streamables = options.delete(:streamables) || target
     options[:target] = target
-    Turbo::StreamsChannel.send(method.to_sym, user, streamables, **options)
+
+    Turbo::StreamsChannel.send(method.to_sym, [user, streamables].compact, **options)
+  end
+
+  # Helper to generate dom_id in jobs
+  def dom_id(record, prefix = nil)
+    ActionView::RecordIdentifier.dom_id(record, prefix)
   end
 end
